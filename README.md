9a8e0df93ae1c04c927e0edea73190f715cebd74
caouecs <caouecs@caouecs.net>
2013-05-30 01:09:04 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,4 +1,22 @@
 Laravel4-lang
 =============
 
-List of languages for Laravel4
+#### English version
+
+You can find in this repository the lang files of Laravel4, for French and others.
+
+To add languages in your application, it's easy, you clone this repository, and you copy the folders of languages in *app/lang*.
+
+You can propose new languages and help to verify pull requests.
+
+---
+
+#### Version française
+
+Ce dépôt permet de récupérer facilement les fichiers de traduction de Laravel v4 en version française et autres.
+
+Vous avez juste à cloner ce dépôt, et mettre le répertoire du ou des langues que vous avez besoin, ainsi que son contenu, dans *app/lang*.
+
+Tout simplement !
+
+Vous pouvez aussi participer à la relecture des fichiers, ou proposez une autre langue.
---
+++ fr/validation.php
@@ -0,0 +1,93 @@
+<?php
+
+return array(
+
+    /*
+    |--------------------------------------------------------------------------
+    | Validation Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines contain the default error messages used by
+    | the validator class. Some of these rules have multiple versions such
+    | such as the size rules. Feel free to tweak each of these messages.
+    |
+    */
+
+    "accepted"         => "Le champ :attribute doit être accepté.",
+    "active_url"       => "Le champ :attribute n'est pas une URL valide.",
+    "after"            => "Le champ :attribute doit être une date postérieure au :date.",
+    "alpha"            => "Le champ :attribute doit seulement contenir des lettres.",
+    "alpha_dash"       => "Le champ :attribute doit seulement contenir des lettres, des chiffres et des tirets.",
+    "alpha_num"        => "Le champ :attribute doit seulement contenir des chiffres et des lettres.",
+    "before"           => "Le champ :attribute doit être une date antérieure au :date.",
+    "between"          => array(
+        "numeric" => "Le champ :attribute doit être entre :min et :max.",
+        "file"    => "Le champ :attribute doit être entre :min et :max kilobytes.",
+        "string"  => "Le champ :attribute doit être entre :min et :max caractères.",
+    ),
+    "confirmed"        => "Le champ de confirmation :attribute ne correspond pas.",
+    "date"             => "Le champ :attribute n'est pas une date valide.",
+    "date_format"      => "Le champ :attribute ne correspond pas au format :format.",
+    "different"        => "Les champ :attribute et :other doivent être différents.",
+    "digits"           => "Le champ :attribute doit avoir :digits chiffres.",
+    "digits_between"   => "Le champ :attribute doit avoir entre :min and :max chiffres.",
+    "email"            => "Le format du champ :attribute est invalide.",
+    "exists"           => "Le champ :attribute sélectionné est invalide.",
+    "image"            => "Le champ :attribute doit être une image.",
+    "in"               => "Le champ :attribute est invalide.",
+    "integer"          => "Le champ :attribute doit être un entier.",
+    "ip"               => "Le champ :attribute doit être une adresse IP valide.",
+    "max"              => array(
+        "numeric" => "La valeur de :attribute ne peut être supérieure à :max.",
+        "file"    => "Le fichier :attribute ne peut être plus gros que :max kilobytes.",
+        "string"  => "Le texte de :attribute ne peut contenir plus de :max caractères.",
+    ),
+    "mimes"            => "Le champ :attribute doit être un fichier de type : :values.",
+    "min"              => array(
+        "numeric" => "La valeur de :attribute doit être inférieure à :min.",
+        "file"    => "Le fichier :attribute doit être plus que gros que :min kilobytes.",
+        "string"  => "Le texte :attribute doit contenir au moins :min caractères.",
+    ),
+    "not_in"           => "Le champ :attribute sélectionné n'est pas valide.",
+    "numeric"          => "Le champ :attribute doit contenir un nombre.",
+    "regex"            => "Le format du champ :attribute est invalide.",
+    "required"         => "Le champ :attribute est obligatoire.",
+    "required_if"      => "Le champ :attribute est obligatoire quand la valeur de :other est :value.",
+    "required_with"    => "Le champ :attribute est obligatoire quand :values est présent.",
+    "required_without" => "Le champ :attribute est obligatoire quand :values n'est pas présent.",
+    "same"             => "Les champs :attribute et :other doivent être identiques.",
+    "size"             => array(
+        "numeric" => "La taille de la valeur de :attribute doit être :size.",
+        "file"    => "La taille du fichier de :attribute doit être de :size kilobytes.",
+        "string"  => "Le texte de :attribute doit contenir :size caractères.",
+    ),
+    "unique"           => "La valeur du champ :attribute est déjà utilisée.",
+    "url"              => "Le format de l'URL de :attribute n'est pas valide.",
+
+    /*
+    |--------------------------------------------------------------------------
+    | Custom Validation Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | Here you may specify custom validation messages for attributes using the
+    | convention "attribute.rule" to name the lines. This makes it quick to
+    | specify a specific custom language line for a given attribute rule.
+    |
+    */
+
+    'custom' => array(),
+
+    /*
+    |--------------------------------------------------------------------------
+    | Custom Validation Attributes
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used to swap attribute place-holders
+    | with something more reader friendly such as E-Mail Address instead
+    | of "email". This simply helps us make messages a little cleaner.
+    |
+    */
+
+    'attributes' => array(),
+
+);

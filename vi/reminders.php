051e1fc2b68a5382f933276f5f10586918c917ef
anhsaker <anhsk.ohbo@gmail.com>
2013-07-30 03:01:37 +0700

    Create reminders.php

---
+++ vi/reminders.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+ * Vietnamese Language for Laravel 4
+ * Translate by anhsk.ohbo@gmail.com
+ *
+ */
+
+return array(
+
+        /*
+	|--------------------------------------------------------------------------
+	| Password Reminder Language Lines
+	|--------------------------------------------------------------------------
+	|
+	| The following language lines are the default lines which match reasons
+	| that are given by the password broker for a password update attempt
+	| has failed, such as for an invalid token or invalid new password.
+	|
+	*/
+
+	// "password" => "Passwords must be six characters and match the confirmation.",
+	"password" => "Passwords must be six characters and match the confirmation.",
+
+	// "user"     => "We can't find a user with that e-mail address.",	
+	"user"     => "We can't find a user with that e-mail address.",
+
+	// "token"    => "This password reset token is invalid.",
+	"token"    => "This password reset token is invalid.",
+
+);

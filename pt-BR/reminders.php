397ea91ed727f0925cff26e1f84e50a451f38472
caouecs <caouecs@caouecs.net>
2013-07-30 09:51:45 +0200

    Indentation pt-br

--- pt-BR/reminders.php
+++ pt-BR/reminders.php
@@ -2,19 +2,19 @@
 
 return array(
 
-	/*
-	|--------------------------------------------------------------------------
-	| Password Reminder Language Lines
-	|--------------------------------------------------------------------------
-	|
-	| The following language lines are the default lines which match reasons
-	| that are given by the password broker for a password update attempt
-	| has failed, such as for an invalid token or invalid new password.
-	|
-	*/
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reminder Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
     "password" => "A senha deverá conter pelo menos seis carateres e ser igual à confirmação.",
 
-	"user"     => "Não conseguimos encontrar nenhum usuário com o endereço de email indicado.",
+    "user"     => "Não conseguimos encontrar nenhum usuário com o endereço de email indicado.",
 
     "token"    => "Este código de recuperação da senha é inválido.",
 

77ca809983e40db2cb23bf0fee7a1bfa23b72f4e
Ammar Alakkad <am.alakkad@gmail.com>
2013-12-15 19:29:46 +0200

    sync & add new item to reminders.php

--- ar/pagination.php
+++ ar/pagination.php
@@ -2,29 +2,8 @@
 
 return array(
 
-<<<<<<< HEAD
-	/*
-=======
-    /*
->>>>>>> 528e054d05c39b30c30ce4aadb55e787d327b1f9
-	|--------------------------------------------------------------------------
-	| Pagination Language Lines
-	|--------------------------------------------------------------------------
-	|
-	| The following language lines are used by the paginator library to build
-	| the simple pagination links. You are free to change them to anything
-	| you want to customize your views to better match your application.
-	|
-	*/
-
-<<<<<<< HEAD
-	'previous' => '&laquo; السابق',
-
-	'next'     => 'التالي &raquo;',
-=======
     'previous' => '&laquo; السابق',
 
     'next'     => 'التالي &raquo;',
->>>>>>> 528e054d05c39b30c30ce4aadb55e787d327b1f9
 
 );

326e13009a162179c95dd4868951158f2b30c3cb
caouecs <caouecs@caouecs.net>
2014-01-10 10:14:21 +0100

    Update Indonesian with Tabs by spaces

--- id/reminders.php
+++ id/reminders.php
@@ -2,23 +2,23 @@
 
 return array(
 
-	/*
-	|--------------------------------------------------------------------------
-	| Password Reminder Language Lines
-	|--------------------------------------------------------------------------
-	|
-	| The following language lines are the default lines which match reasons
-	| that are given by the password broker for a password update attempt
-	| has failed, such as for an invalid token or invalid new password.
-	|
-	*/
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reminder Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
 
-	"password" 	=> "Kata sandi harus minimal enam karakter dan cocok dengan konfirmasi.",
+    "password"  => "Kata sandi harus minimal enam karakter dan cocok dengan konfirmasi.",
 
-	"user" 		=> "Kami tidak dapat menemukan pengguna dengan alamat surel tersebut.",
+    "user"      => "Kami tidak dapat menemukan pengguna dengan alamat surel tersebut.",
 
-	"token" 	=> "Kata sandi token pengaturan ulang tidak sah.",
+    "token"     => "Kata sandi token pengaturan ulang tidak sah.",
 
-	"sent" 		=> "Pengingat kata sandi dikirim!",
+    "sent"      => "Pengingat kata sandi dikirim!",
 
 );
--- id/validation.php
+++ id/validation.php
@@ -2,97 +2,97 @@
 
 return array(
 
-	/*
-	|--------------------------------------------------------------------------
-	| Validation Language Lines
-	|--------------------------------------------------------------------------
-	|
-	| The following language lines contain the default error messages used by
-	| the validator class. Some of these rules have multiple versions such
-	| as the size rules. Feel free to tweak each of these messages here.
-	|
-	*/
+    /*
+    |--------------------------------------------------------------------------
+    | Validation Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines contain the default error messages used by
+    | the validator class. Some of these rules have multiple versions such
+    | as the size rules. Feel free to tweak each of these messages here.
+    |
+    */
 
-	"accepted"         => "Isian :attribute harus diterima.",
-	"active_url"       => "Isian :attribute bukan URL yang sah.",
-	"after"            => "Isian :attribute harus tanggal setelah :date.",
-	"alpha"            => "Isian :attribute hanya boleh berisi huruf.",
-	"alpha_dash"       => "Isian :attribute hanya boleh berisi huruf, angka, dan strip.",
-	"alpha_num"        => "Isian :attribute hanya boleh berisi huruf dan angka.",
-	"array"            => "Isian :attribute harus berupa sebuah array.",
-	"before"           => "Isian :attribute harus tanggal sebelum :date.",
-	"between"          => array(
-		"numeric" => "Isian :attribute harus antara :min dan :max.",
-		"file"    => "Isian :attribute harus antara :min dan :max kilobytes.",
-		"string"  => "Isian :attribute harus antara :min dan :max karakter.",
-		"array"   => "Isian :attribute harus antara :min dan :max item.",
-	),
-	"confirmed"        => "Konfirmasi :attribute tidak cocok.",
-	"date"             => "Isian :attribute bukan tanggal yang sah.",
-	"date_format"      => "Isian :attribute tidak cocok dengan format :format.",
-	"different"        => "Isian :attribute dan :other harus berbeda.",
-	"digits"           => "Isian :attribute harus berupa angka :digits.",
-	"digits_between"   => "Isian :attribute harus antara angka :min dan :max.",
-	"email"            => "Format isian :attribute tidak sah.",
-	"exists"           => "Isian :attribute yang dipilih tidak sah.",
-	"image"            => "Isian :attribute harus berupa gambar.",
-	"in"               => "Isian :attribute yang dipilih tidak sah.",
-	"integer"          => "Isian :attribute harus merupakan bilangan bulat.",
-	"ip"               => "Isian :attribute harus berupa alamat IP yang sah.",
-	"max"              => array(
-		"numeric" => "Isian :attribute seharusnya tidak lebih dari :max.",
-		"file"    => "Isian :attribute seharusnya tidak lebih dari :max kilobytes.",
-		"string"  => "Isian :attribute seharusnya tidak lebih dari :max karakter.",
-		"array"   => "Isian :attribute seharusnya tidak lebih dari :max item.",
-	),
-	"mimes"            => "Isian :attribute harus dokumen berjenis : :values.",
-	"min"              => array(
-		"numeric" => "Isian :attribute harus minimal :min.",
-		"file"    => "Isian :attribute harus minimal :min kilobytes.",
-		"string"  => "Isian :attribute harus minimal :min karakter.",
-		"array"   => "Isian :attribute harus minimal :min item.",
-	),
-	"not_in"           => "Isian :attribute yang dipilih tidak sah.",
-	"numeric"          => "Isian :attribute harus berupa angka.",
-	"regex"            => "The :attribute format is invalid.",
-	"required"         => "Bidang isian :attribute wajib diisi.",
-	"required_if"      => "Bidang isian :attribute wajib diisi ketika :other adalah :value.",
-	"required_with"    => "Bidang isian :attribute wajib diisi ketika terdapat :values.",
-	"required_without" => "Bidang isian :attribute wajib diisi ketika tidak terdapat :values.",
-	"same"             => "Isian :attribute dan :other harus sama.",
-	"size"             => array(
-		"numeric" => "Isian :attribute harus berukuran :size.",
-		"file"    => "Isian :attribute harus berukuran :size kilobyte.",
-		"string"  => "Isian :attribute harus berukuran :size karakter.",
-		"array"   => "Isian :attribute harus mengandung :size item.",
-	),
-	"unique"           => "Isian :attribute sudah ada sebelumnya.",
-	"url"              => "Format isian :attribute tidak sah.",
+    "accepted"         => "Isian :attribute harus diterima.",
+    "active_url"       => "Isian :attribute bukan URL yang sah.",
+    "after"            => "Isian :attribute harus tanggal setelah :date.",
+    "alpha"            => "Isian :attribute hanya boleh berisi huruf.",
+    "alpha_dash"       => "Isian :attribute hanya boleh berisi huruf, angka, dan strip.",
+    "alpha_num"        => "Isian :attribute hanya boleh berisi huruf dan angka.",
+    "array"            => "Isian :attribute harus berupa sebuah array.",
+    "before"           => "Isian :attribute harus tanggal sebelum :date.",
+    "between"          => array(
+        "numeric" => "Isian :attribute harus antara :min dan :max.",
+        "file"    => "Isian :attribute harus antara :min dan :max kilobytes.",
+        "string"  => "Isian :attribute harus antara :min dan :max karakter.",
+        "array"   => "Isian :attribute harus antara :min dan :max item.",
+    ),
+    "confirmed"        => "Konfirmasi :attribute tidak cocok.",
+    "date"             => "Isian :attribute bukan tanggal yang sah.",
+    "date_format"      => "Isian :attribute tidak cocok dengan format :format.",
+    "different"        => "Isian :attribute dan :other harus berbeda.",
+    "digits"           => "Isian :attribute harus berupa angka :digits.",
+    "digits_between"   => "Isian :attribute harus antara angka :min dan :max.",
+    "email"            => "Format isian :attribute tidak sah.",
+    "exists"           => "Isian :attribute yang dipilih tidak sah.",
+    "image"            => "Isian :attribute harus berupa gambar.",
+    "in"               => "Isian :attribute yang dipilih tidak sah.",
+    "integer"          => "Isian :attribute harus merupakan bilangan bulat.",
+    "ip"               => "Isian :attribute harus berupa alamat IP yang sah.",
+    "max"              => array(
+        "numeric" => "Isian :attribute seharusnya tidak lebih dari :max.",
+        "file"    => "Isian :attribute seharusnya tidak lebih dari :max kilobytes.",
+        "string"  => "Isian :attribute seharusnya tidak lebih dari :max karakter.",
+        "array"   => "Isian :attribute seharusnya tidak lebih dari :max item.",
+    ),
+    "mimes"            => "Isian :attribute harus dokumen berjenis : :values.",
+    "min"              => array(
+        "numeric" => "Isian :attribute harus minimal :min.",
+        "file"    => "Isian :attribute harus minimal :min kilobytes.",
+        "string"  => "Isian :attribute harus minimal :min karakter.",
+        "array"   => "Isian :attribute harus minimal :min item.",
+    ),
+    "not_in"           => "Isian :attribute yang dipilih tidak sah.",
+    "numeric"          => "Isian :attribute harus berupa angka.",
+    "regex"            => "The :attribute format is invalid.",
+    "required"         => "Bidang isian :attribute wajib diisi.",
+    "required_if"      => "Bidang isian :attribute wajib diisi ketika :other adalah :value.",
+    "required_with"    => "Bidang isian :attribute wajib diisi ketika terdapat :values.",
+    "required_without" => "Bidang isian :attribute wajib diisi ketika tidak terdapat :values.",
+    "same"             => "Isian :attribute dan :other harus sama.",
+    "size"             => array(
+        "numeric" => "Isian :attribute harus berukuran :size.",
+        "file"    => "Isian :attribute harus berukuran :size kilobyte.",
+        "string"  => "Isian :attribute harus berukuran :size karakter.",
+        "array"   => "Isian :attribute harus mengandung :size item.",
+    ),
+    "unique"           => "Isian :attribute sudah ada sebelumnya.",
+    "url"              => "Format isian :attribute tidak sah.",
 
-	/*
-	|--------------------------------------------------------------------------
-	| Custom Validation Language Lines
-	|--------------------------------------------------------------------------
-	|
-	| Here you may specify custom validation messages for attributes using the
-	| convention "attribute.rule" to name the lines. This makes it quick to
-	| specify a specific custom language line for a given attribute rule.
-	|
-	*/
+    /*
+    |--------------------------------------------------------------------------
+    | Custom Validation Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | Here you may specify custom validation messages for attributes using the
+    | convention "attribute.rule" to name the lines. This makes it quick to
+    | specify a specific custom language line for a given attribute rule.
+    |
+    */
 
-	'custom' => array(),
+    'custom' => array(),
 
-	/*
-	|--------------------------------------------------------------------------
-	| Custom Validation Attributes
-	|--------------------------------------------------------------------------
-	|
-	| The following language lines are used to swap attribute place-holders
-	| with something more reader friendly such as E-Mail Address instead
-	| of "email". This simply helps us make messages a little cleaner.
-	|
-	*/
+    /*
+    |--------------------------------------------------------------------------
+    | Custom Validation Attributes
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used to swap attribute place-holders
+    | with something more reader friendly such as E-Mail Address instead
+    | of "email". This simply helps us make messages a little cleaner.
+    |
+    */
 
-	'attributes' => array(),
+    'attributes' => array(),
 
 );

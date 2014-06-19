f0c1be20453dfa4c49dba5832c9174a669c7892b
miguelbgouveia <miguelbgouveia@hotmail.com>
2014-06-19 15:16:56 +0100

    Update boolean validation
    Just a small correction.

--- pt-BR/validation.php
+++ pt-BR/validation.php
@@ -27,7 +27,7 @@ return array(
         "string"  => "O campo :attribute deverá conter entre :min - :max caracteres.",
         "array"   => "O campo :attribute precisar ter entre :min - :max itens."
     ),
-    "boolean"          => "O campo :attribute deverá ter o valor true or false",
+    "boolean"          => "O campo :attribute deverá ter o valor true ou false",
     "confirmed"        => "A confirmação para o campo :attribute não coincide.",
     "date"             => "O campo :attribute não contém uma data válida.",
     "date_format"      => "A data indicada para o campo :attribute não respeita o formato :format.",

3874b42be655e81d004de7d44642bab2766c2d4e
miguelbgouveia <miguelbgouveia@hotmail.com>
2014-06-19 15:20:12 +0100

    Update required_with_all
    Translate required_with_all to portuguese

--- pt/validation.php
+++ pt/validation.php
@@ -59,7 +59,7 @@ return array(
     "required"         => "É obrigatória a indicação de um valor para o campo :attribute.",
     "required_if"      => "É obrigatória a indicação de um valor para o campo :attribute quando o valor do campo :other é igual a :value.",
     "required_with"    => "É obrigatória a indicação de um valor para o campo :attribute quando :values está presente.",
-    "required_with_all" => "The :attribute field is required when :values is present.",
+    "required_with_all" => "É obrigatória a indicação de um valor para o campo :attribute quando um dos :values está presente.",
     "required_without" => "É obrigatória a indicação de um valor para o campo :attribute quanto :values não está presente.",
     "required_without_all" => "É obrigatória a indicação de um valor para o campo :attribute quando nenhum dos :values está presente.",
     "same"             => "Os campos :attribute e :other deverão conter valores iguais.",

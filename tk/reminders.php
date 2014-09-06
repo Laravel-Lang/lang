3c38927c3ca1e271939b6027f0b397a4675623f4
Isa Ishangulyyev <isa424@users.noreply.github.com>
2014-09-06 04:16:00 +0300

    Create reminders.php

---
+++ tk/reminders.php
@@ -0,0 +1,35 @@
+<?php
+
+/**
+ * Laravel - A PHP Framework For Web Artisans
+ *
+ * @package  Language
+ * @version  4.x
+ * @author   Isa Ishangulyyev <ishankuliyew424@gmail.com>
+ * @link     https://github.com/isa424
+ */
+
+return array(
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reminder Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    "password" => "Şifreler altı karakter olmalı ve onay ile eşleşmelidir.",
+
+    "user"     => "Bu e-mail adresi ile kayıtlı üye bulunamıyor.",
+
+    "token"    => "Şifre sıfırlama bileti geçersiz.",
+
+    "sent"     => "Şifre sıfırlama başarı ile gönderildi!",
+
+    "reset"    => "Password has been reset!",
+
+);

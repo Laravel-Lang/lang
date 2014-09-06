39d4a868ff651640231f2d3ea713c29caf3af3df
Isa Ishangulyyev <isa424@users.noreply.github.com>
2014-09-06 04:13:53 +0300

    Create pagination.php

---
+++ tk/pagination.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * Laravel - A PHP Framework For Web Artisans
+ *
+ * @package  Language
+ * @version  4.x
+ * @author   Isa Ishangulyyev <ishankuliyew424@gmail.com>
+ * @link     https://github.com/isa424
+ */
+
+return array(
+
+    /*
+    |--------------------------------------------------------------------------
+    | Pagination Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used by the paginator library to build
+    | the simple pagination links. You are free to change them to anything
+    | you want to customize your views to better match your application.
+    |
+    */
+
+    'previous' => '&laquo; Öňki',
+
+    'next'     => 'Indiki &raquo;',
+
+);

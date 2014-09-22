a43cbdd5eb0a408d808db5508e6033537313c9e8
Nikos Gr <grrnikos@yahoo.gr>
2014-09-22 13:16:02 +0300

    update el/validation.php

--- el/validation.php
+++ el/validation.php
@@ -15,7 +15,7 @@ return array(
 
     "accepted"         => "Το πεδίο :attribute πρέπει να γίνει αποδεκτό.",
     "active_url"       => "Το πεδίο :attribute δεν είναι αποδεκτή διεύθυνση URL.",
-    "after"            => "Το πεδίο :attribute μπορεί να είναι μία ημερομηνία μετά από :date.",
+    "after"            => "Το πεδίο :attribute πρέπει να είναι μία ημερομηνία μετά από :date.",
     "alpha"            => "Το πεδίο :attribute μπορεί να περιέχει μόνο γράμματα.",
     "alpha_dash"       => "Το πεδίο :attribute μπορεί να περιέχει μόνο γράμματα, αριθμούς, και παύλες.",
     "alpha_num"        => "Το πεδίο :attribute μπορεί να περιέχει μόνο γράμματα και αριθμούς.",
@@ -27,26 +27,26 @@ return array(
         "string"  => "Το πεδίο :attribute πρέπει να είναι μεταξύ :min - :max χαρακτήρες.",
         "array"   => "Το πεδίο :attribute πρέπει να έχει μεταξύ :min - :max αντικείμενα."
     ),
-    "boolean"          => "The :attribute field must be true or false",
+    "boolean"          => "Το πεδίο :attribute πρέπει να είναι true ή false.",
     "confirmed"        => "Η επιβεβαίωση του :attribute δεν ταιριάζει.",
     "date"             => "Το πεδίο :attribute δεν αποτελεί αποδεκτή ημερομηνία.",
     "date_format"      => "Το πεδίο :attribute δεν είναι της μορφής :format.",
     "different"        => "Το πεδίο :attribute και :other πρέπει να είναι διαφορετικά.",
     "digits"           => "Το πεδίο :attribute πρέπει να είναι :digits ψηφία.",
-    "digits_between"   => "Το πεδίο :attribute πρέπει να είναι between :min and :max ψηφία.",
-    "email"            => "Το πεδίο :attribute δεν είναι αποδεκτή διεύθυνση email.",
+    "digits_between"   => "Το πεδίο :attribute πρέπει να είναι μεταξύ :min και :max ψηφία.",
+    "email"            => "Το πεδίο :attribute πρέπει να είναι μία έγκυρη διεύθυνση email.",
     "exists"           => "Το επιλεγμένο :attribute δεν είναι έγκυρο.",
     "image"            => "Το πεδίο :attribute πρέπει να είναι εικόνα.",
     "in"               => "Το επιλεγμένο :attribute δεν είναι έγκυρο.",
     "integer"          => "Το πεδίο :attribute πρέπει να είναι ακέραιος.",
-    "ip"               => "Το πεδίο :attribute πρέπει να είναι αποδεκτή δ/νση IP.",
+    "ip"               => "Το πεδίο :attribute πρέπει να είναι μία έγκυρη διεύθυνση IP.",
     "max"              => array(
         "numeric" => "Το πεδίο :attribute δεν μπορεί να είναι μεγαλύτερο από :max.",
         "file"    => "Το πεδίο :attribute δεν μπορεί να είναι μεγαλύτερό :max kilobytes.",
         "string"  => "Το πεδίο :attribute δεν μπορεί να είναι μεγαλύτερό :max χαρακτήρες.",
         "array"   => "Το πεδίο :attribute δεν μπορεί να έχει περισσότερα από :max αντικείμενα."
     ),
-    "mimes"            => "Το πεδίο :attribute πρέπει να είναι αρχείο με τύπο: :values.",
+    "mimes"            => "Το πεδίο :attribute πρέπει να είναι αρχείο τύπου: :values.",
     "min"              => array(
         "numeric" => "Το πεδίο :attribute πρέπει να είναι τουλάχιστον :min.",
         "file"    => "Το πεδίο :attribute πρέπει να είναι τουλάχιστον :min kilobytes.",
@@ -59,9 +59,9 @@ return array(
     "required"         => "Το πεδίο :attribute είναι απαραίτητο.",
     "required_if"      => "Το πεδίο :attribute είναι απαραίτητο όταν το πεδίο :other είναι :value.",
     "required_with"    => "Το πεδίο :attribute είναι απαραίτητο όταν υπάρχει :values.",
-    "required_with_all" => "The :attribute field is required when :values is present.",
+    "required_with_all" => "Το πεδίο :attribute είναι απαραίτητο όταν υπάρχουν :values.",
     "required_without" => "Το πεδίο :attribute είναι απαραίτητο όταν δεν υπάρχει :values.",
-    "required_without_all" => "The :attribute field is required when none of :values are present.",
+    "required_without_all" => "Το πεδίο :attribute είναι απαραίτητο όταν δεν υπάρχει κανένα από :values.",
     "same"             => "Τα πεδία :attribute και :other πρέπει να είναι ίδια.",
     "size"             => array(
         "numeric" => "Το πεδίο :attribute πρέπει να είναι :size.",
@@ -69,7 +69,7 @@ return array(
         "string"  => "Το πεδίο :attribute πρέπει να είναι :size χαρακτήρες.",
         "array"   => "Το πεδίο :attribute πρέπει να περιέχει :size αντικείμενα."
     ),
-    "timezone"         => "Το πεδίο :attribute πρέπει να είναι μία έγκυρη ζώνη ώρας.",
+    "timezone"         => "Το πεδίο :attribute πρέπει να είναι μία αποδεκτή ζώνη ώρας.",
     "unique"           => "Το πεδίο :attribute έχει ήδη εκχωρηθεί.",
     "url"              => "Το πεδίο :attribute δεν είναι μία αποδεκτή διεύθυνση URL.",
 

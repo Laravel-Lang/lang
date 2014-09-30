b79afe286bb5f4cdb01ba5b91c8f634fa668ef83
Isa Ishangulyyev <isa424@users.noreply.github.com>
2014-09-30 20:11:27 +0300

    Update validation.php

--- tk/validation.php
+++ tk/validation.php
@@ -43,7 +43,7 @@ return array(
     "different"             => ":attribute bilen :other birbirinden tapawutly bolmalydyr.",
     "digits"                => ":attribute :digits san bolmalydyr.",
     "digits_between"        => ":attribute :min bilen :max arasynda san bolmalydyr.",
-    "email"                 => ":attribute formaty dogry däl.",
+    "email"                 => ":attribute formaty ýalňyş.",
     "exists"                => "Saýlanan :attribute ýalňyş.",
     "image"                 => ":attribute surat bolmalydyr.",
     "in"                    => ":attribute mukdary ýalňyş.",

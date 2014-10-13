49a971a08799bea5dc0b1202e94c8bb62217fd78
mrakodol <mrakodol@gmail.com>
2014-10-13 21:55:05 +0200

    Update timezone

--- sr/validation.php
+++ sr/validation.php
@@ -69,7 +69,7 @@ return [
         "string"  => "Polje :attribute mora biti :size karaktera.",
         "array"   => "Polje :attribute mora sadržati :size stavki."
     ],
-    "timezone"         => "The :attribute must be a valid zone.",
+    "timezone"         => ":attribute mora biti ispravna vremenska zona.",
     "unique"           => "Polje :attribute već postoji.",
     "url"              => "Format polja :attribute ne važi.",
 

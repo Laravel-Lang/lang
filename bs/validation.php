664656aadfae7df0e802bca61e47dedf7f09ed93
mrakodol <mrakodol@gmail.com>
2014-10-13 21:53:10 +0200

    Update timezone

--- bs/validation.php
+++ bs/validation.php
@@ -68,7 +68,7 @@ return [
         "string"        => "Polje :attribute mora biti :size karaktera.",
         "array"         => "Polje :attribute mora biti :size karaktera."
     ],
-    "timezone"          => "The :attribute must be a valid zone.",
+    "timezone"          => ":attribute mora biti ispravna vremenska zona.",
     "unique"            => "Polje :attribute već postoji.",
     "url"               => "Format polja :attribute nije validan.",
 

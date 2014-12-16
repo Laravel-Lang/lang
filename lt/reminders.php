0239950f08d39e0c1a6dc942d7346a37d96237d5
Caouecs <caouecs@caouecs.net>
2014-12-16 21:13:15 +0100

    Indentation by space

--- lt/reminders.php
+++ lt/reminders.php
@@ -2,25 +2,25 @@
 
 return array(
 
-	/*
-	|--------------------------------------------------------------------------
-	| Slaptažodžio priminimo kalbos eilutės
-	|--------------------------------------------------------------------------
-	|
-	| Sekančios kalbos eilutės yra numatytos elutės, atitinkančios priežastims,
-	| pateikiamoms slatažodžių tarpininko, kai nepavyksta slaptažodžio atnaujinimo  
-	| bandymas, tokioms kaip negaliojanti žymė ar neteisingas naujas slaptažodis..
-	|
-	*/
+    /*
+    |--------------------------------------------------------------------------
+    | Slaptažodžio priminimo kalbos eilutės
+    |--------------------------------------------------------------------------
+    |
+    | Sekančios kalbos eilutės yra numatytos elutės, atitinkančios priežastims,
+    | pateikiamoms slatažodžių tarpininko, kai nepavyksta slaptažodžio atnaujinimo
+    | bandymas, tokioms kaip negaliojanti žymė ar neteisingas naujas slaptažodis..
+    |
+    */
 
-	"password" => "Slaptažodis turi būti bent šešių simbolių ir sutapti su patvirtinimu.",
+    "password" => "Slaptažodis turi būti bent šešių simbolių ir sutapti su patvirtinimu.",
 
-	"user" => "Vartotojas su tokiu el. pašu nerastas.",
+    "user" => "Vartotojas su tokiu el. pašu nerastas.",
 
-	"token" => "Šis slaptažodžio raktas yra neteisingas.",
+    "token" => "Šis slaptažodžio raktas yra neteisingas.",
 
-	"sent" => "Slaptažodžio priminimas išsiųstas!",
+    "sent" => "Slaptažodžio priminimas išsiųstas!",
 
-	"reset" => "Nustatytas naujas slaptažodis!",
+    "reset" => "Nustatytas naujas slaptažodis!",
 
 );

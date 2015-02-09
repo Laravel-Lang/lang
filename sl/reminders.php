991018f895cc98caf8e8f893bf5b7f4f05b3db37
extra90 <mitja.rogl@gmail.com>
2015-02-09 15:25:43 +0100

    Slovenia lang
    Adds initial slovenian language pack.

---
+++ sl/reminders.php
@@ -0,0 +1,24 @@
+<?php
+
+return array(
+
+	/*
+	|--------------------------------------------------------------------------
+	| Password Reminder Language Lines
+	|--------------------------------------------------------------------------
+	|
+	| The following language lines are the default lines which match reasons
+	| that are given by the password broker for a password update attempt
+	| has failed, such as for an invalid token or invalid new password.
+	|
+	*/
+
+	"password" => "Geslo mora biti dolgo vsaj šest znakov in se mora ujemati z potrditvenim geslom.",
+
+	"user" => "Ne moremo najti uporabnika s tem e-poštnim naslovom.",
+
+	"token" => "Ponastavitveni žeton je neveljaven.",
+
+	"sent" => "Opomnik za geslo poslano!",
+
+);

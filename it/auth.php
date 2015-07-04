db465f9e3430edd2951d92eae4dfbcfd7fac0d77
caouecs <caouecs@caouecs.net>
2015-07-05 01:01:27 +0200

    feature: Italian version of auth file

--- it/auth.php
+++ it/auth.php
@@ -11,5 +11,5 @@ return [
     |
     */
     'failed' => 'These credentials do not match our records.',
-    'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
+    'throttle' => 'Troppi tentativi di accesso. Riprova tra :seconds secondi.',
 ];

7cb32727d3a706cf8c24795aed3098381d7faef1
Hans Ott <hansott@hotmail.be>
2015-07-09 00:30:50 +0200

    NL: Add authentication translations.

--- nl/auth.php
+++ nl/auth.php
@@ -10,6 +10,6 @@ return [
     | these language lines according to your application's requirements.
     |
     */
-    'failed' => 'These credentials do not match our records.',
-    'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
+    'failed' => 'De combinatie van e-mailadres en wachtwoord is niet geldig.',
+    'throttle' => 'Teveel gefaalde login pogingen. Probeer het over :seconds seconden nogmaals.',
 ];

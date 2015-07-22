5d2d993d1979463f8c115501e9f33e5dc8859fdf
Rafael <rafi.93.zgz@gmail.com>
2015-07-22 09:45:16 +0200

    Update auth.php

--- ca/auth.php
+++ ca/auth.php
@@ -11,5 +11,5 @@ return [
     |
     */
     'failed' => 'Aquestes credencials no concorden amb els nostres registres.',
-    'throttle' => 'Heu superat el nombre màxim d\'intents de login. Per favor, torna a intentar-ho en :seconds segons.',
+    'throttle' => 'Heu superat el nombre màxim d\'intents d\'accés. Per favor, torna a intentar-ho en :seconds segons.',
 ];

63d6a3ad7abada279e88075cba857da52a87ac04
Mihai Zaharie <mihai@zaharie.ro>
2015-09-19 11:47:28 +0300

    Fix typo in Romanian translation

--- ro/passwords.php
+++ ro/passwords.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'password' => 'Parolela trebuie să fie de cel puțin șase caractere și să se potrivească cu cea de confirmare.',
+    'password' => 'Parola trebuie să fie de cel puțin șase caractere și să se potrivească cu cea de confirmare.',
     'reset' => 'Parola a fost resetată!',
     'sent' => 'Am trimis un e-mail cu link-ul de resetare a parolei!',
     'token' => 'Codul de resetare a parolei este greșit.',

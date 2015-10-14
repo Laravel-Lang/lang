bf056a54e9cca17278ff7a81bcfa7ae11772ee47
KwangSeob Jeong <lesstif@gmail.com>
2015-10-14 14:03:38 +0900

    transalation korean auth messages.

--- ko/auth.php
+++ ko/auth.php
@@ -10,6 +10,6 @@ return [
     | these language lines according to your application's requirements.
     |
     */
-    'failed' => 'These credentials do not match our records.',
-    'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
+    'failed' => '제출된 인증 정보가 레코드와 일치하지 않습니다.',
+    'throttle' => '너무 많은 로그인을 시도하였습니다. :seconds 초 후에 다시 시도하십시요.',
 ];

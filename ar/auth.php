2fc5c57f8afa0b7fd761f7b4440f9559e1456ffb
abdullah-ghanem <3bdullah.ghanem@gmail.com>
2015-11-02 14:37:36 +0200

    throttle has been translated into arabic

--- ar/auth.php
+++ ar/auth.php
@@ -12,5 +12,5 @@ return [
     |
     */
     'failed' => 'These credentials do not match our records.',
-    'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
+    'throttle' => 'عدد كبير جدا من محاولات الدخول. يرجى المحاولة مرة أخرى بعد :seconds ثواني.',
 ];

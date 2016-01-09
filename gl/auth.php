ba7ca9f1d0068e1755b590d9e307520fb30e85dd
Jesús Amieiro <amieiro@gmail.com>
2016-01-09 13:30:02 +0100

    Translate the auth.php file to the Galician language (gl ISO 639-1)

--- gl/auth.php
+++ gl/auth.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'failed' => 'These credentials do not match our records.',
-    'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
+    'failed' => 'Estas credenciais non coinciden cos nosos rexistros.',
+    'throttle' => 'Demasiados intentos de acceso. Por favor inténteo de novo en :seconds segundos.',
 
 ];

64305b6ca97712671f6147dfa14f4e83b3641165
Jesús Amieiro <amieiro@gmail.com>
2016-01-10 04:13:39 +0100

    Resolve some small problems in the format

--- gl/passwords.php
+++ gl/passwords.php
@@ -17,6 +17,6 @@ return [
     'reset' => 'O teu contrasinal foi restablecido!',
     'sent' => 'Enviámosche por correo electrónico o enlace de restablecemento do contrasinal!',
     'token' => 'Este token de restablecemento do contrasinal non é válido.',
-    'user' => "Non podemos atopar un usuario con ese correo electrónico.",
+    'user' => 'Non podemos atopar un usuario con ese correo electrónico.',
 
 ];

b77aef694d78271261562b20ca5f25a252047652
victorarthur <hello@victorarthur.com>
2016-01-18 10:40:42 +0100

    Update auth.php
    Hungarian translation of Auth.php.

--- hu/auth.php
+++ hu/auth.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'failed'   => 'These credentials do not match our records.',
-    'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
+    'failed'   => 'A megadott adatokkal megegyező felhasználó nem található az adatbázisunkban.',
+    'throttle' => 'Túl sok próbálkozás. Kérjü próbálja újra :seconds másodperc múlva.',
 
 ];

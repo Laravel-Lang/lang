0aba9b16c40a74a798b752815ef234e3ff47bb6b
ARCANEDEV <arcanedev.maroc@gmail.com>
2016-03-20 16:34:10 +0000

    Updating TODO.md generator script

--- script/todo.php
+++ script/todo.php
@@ -1,50 +1,101 @@
 <?php
 
-// Get list of languages
-$glob = scandir(__DIR__.'/..');
+class TodoGenerator
+{
+    /* ------------------------------------------------------------------------------------------------
+     |  Properties
+     | ------------------------------------------------------------------------------------------------
+     */
+    /** @var string */
+    protected $basePath;
 
-$languages = null;
+    /** @var array */
+    protected $excluded = [];
 
-foreach ($glob as $file) {
-    if (is_dir(__DIR__.'/../'.$file) && (!in_array($file, ['.', '..', '.git', '.phpintel', 'en', 'script']))) {
-        $languages[] = $file;
+    /** @var string */
+    protected $output = '';
+
+    /* ------------------------------------------------------------------------------------------------
+     |  Constructor
+     | ------------------------------------------------------------------------------------------------
+     */
+    public function __construct($basePath, $excluded = [])
+    {
+        $this->basePath = realpath($basePath);
+        $this->excluded = $excluded;
+        $this->output = '';
+        $this->load();
     }
-}
 
-// Get English version
-$english = [
-    'auth'       => include(__DIR__.'/../en/auth.php'),
-    'pagination' => include(__DIR__.'/../en/pagination.php'),
-    'passwords'  => include(__DIR__.'/../en/passwords.php'),
-    'validation' => include(__DIR__.'/../en/validation.php'),
-];
+    public static function make($basePath, $excluded = [])
+    {
+        return new self($basePath, $excluded);
+    }
 
-$text = "# Todo list\n\n";
+    /* ------------------------------------------------------------------------------------------------
+     |  Main Functions
+     | ------------------------------------------------------------------------------------------------
+     */
+    private function load()
+    {
+        // Get English version
+        $english = $this->getTranslations(__DIR__.'/en');
+        $languages = $this->getLanguages();
 
-// Return diff language by language
-foreach ($languages as $language) {
-    $text .= "\n * ".$language.":\n";
+        $this->output = "# Todo list\n\n";
+        $this->compareTranslations($english, $languages);
+    }
 
-    $current = [
-        'auth'       => include(__DIR__.'/../'.$language.'/auth.php'),
-        'pagination' => include(__DIR__.'/../'.$language.'/pagination.php'),
-        'passwords'  => include(__DIR__.'/../'.$language.'/passwords.php'),
-        'validation' => include(__DIR__.'/../'.$language.'/validation.php'),
-    ];
+    private function getTranslations($dir)
+    {
+        return [
+            'auth'       => include("$dir/auth.php"),
+            'pagination' => include("$dir/pagination.php"),
+            'passwords'  => include("$dir/passwords.php"),
+            'validation' => include("$dir/validation.php"),
+        ];
+    }
 
-    foreach ($english as $key => $values) {
-        foreach ($values as $key2 => $value2) {
-            if ($key2 != 'custom' && $key2 != 'attributes') {
-                if (!isset($current[$key][$key2])) {
-                    $text .= '    * '.$key.' : '.$key2." : not present\n";
-                } elseif ($current[$key][$key2] == $english[$key][$key2]) {
-                    $text .= '    * '.$key.' : '.$key2."\n";
+    private function getLanguages()
+    {
+        $directories = glob("{$this->basePath}/*", GLOB_ONLYDIR);
+
+        $languages = array_map(function ($dir) {
+            $name = basename($dir);
+
+            return in_array($name, $this->excluded) ? null : $name;
+        }, $directories);
+
+        return array_filter($languages);
+    }
+
+    private function compareTranslations($default, $languages)
+    {
+        // Return diff language by language
+        foreach ($languages as $language) {
+            $this->output .= "\n * ".$language.":\n";
+            $current = $this->getTranslations("{$this->basePath}/{$language}");
+
+            foreach ($default as $key => $values) {
+                foreach ($values as $key2 => $value2) {
+                    if (in_array($key2, ['custom', 'attributes'])) {
+                        continue;
+                    }
+
+                    if (!isset($current[$key][$key2])) {
+                        $this->output .= '    * '.$key.' : '.$key2." : not present\n";
+                    } elseif ($current[$key][$key2] == $default[$key][$key2]) {
+                        $this->output .= '    * '.$key.' : '.$key2."\n";
+                    }
                 }
             }
         }
     }
+
+    public function save($path)
+    {
+        file_put_contents(realpath($path), $this->output);
+    }
 }
 
-$pathTodoMd = __DIR__.'/../todo.md';
-
-file_put_contents($pathTodoMd, $text);
+TodoGenerator::make(__DIR__.'/..', ['script'])->save(__DIR__.'/../todo.md');

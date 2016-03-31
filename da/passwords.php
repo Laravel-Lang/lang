93d823f248eda7f9c53cc6099e14bc9813ef9992
Thomas Wogelius <wogelius@gmail.com>
2016-03-31 21:54:39 +0200

    Fixed errors in the danish translation

--- da/passwords.php
+++ da/passwords.php
@@ -13,10 +13,10 @@ return [
     |
     */
 
-    'password' => 'Adgangskoder skal minimum være 6 tegn og skal være tastet ens i begge felter.',
+    'password' => 'Adgangskode skal minimum være 6 tegn og skal være tastet ens i begge felter.',
     'reset'    => 'Adgangskoden er blevet nulstillet!',
     'sent'     => 'Vi har sendt dig en e-mail til at nulstille din adgangskode!',
     'token'    => 'Koden til nulstilling af adgangskoden er ugyldig.',
-    'user'     => 'Vi kan ikke finde en bruger med den e-mail adresse.',
+    'user'     => 'Vi kan ikke finde en bruger med den e-mailadresse.',
 
 ];

9c662d0dc8f490d2ed8b47a8840c7d5df49fc2eb
laracats <remotemethod@outlook.com>
2016-06-03 14:24:11 +0300

    translation dimensions

--- src/ru/validation.php
+++ src/ru/validation.php
@@ -35,7 +35,7 @@ return [
     'different'            => 'Поля :attribute и :other должны различаться.',
     'digits'               => 'Длина цифрового поля :attribute должна быть :digits.',
     'digits_between'       => 'Длина цифрового поля :attribute должна быть между :min и :max.',
-    'dimensions'           => 'The :attribute has invalid image dimensions.',
+    'dimensions'           => 'Поле :attribute имеет недопустимые размеры изображения.',
     'distinct'             => 'Поле :attribute содержит повторяющееся значение.',
     'email'                => 'Поле :attribute должно быть действительным электронным адресом.',
     'filled'               => 'Поле :attribute обязательно для заполнения.',

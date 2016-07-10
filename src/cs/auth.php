ea258977b1dcb47880008ac2086d0efa7eb40284
Martin Janeček <admin@ikw.cz>
2016-07-10 20:19:11 +0200

    typo

--- src/cs/auth.php
+++ src/cs/auth.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'failed'   => 'Tyto přihlašovací údajě neodpovídají žadnému záznamu.',
+    'failed'   => 'Tyto přihlašovací údaje neodpovídají žadnému záznamu.',
     'throttle' => 'Příliš mnoho pokusů o přihlášení. Zkuste to prosím znovu za :seconds vteřin.',
 
 ];

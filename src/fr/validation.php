4324cd2bdff75ed27e91c0fcd9efeb6eb272ea66
caouecs <caouecs@caouecs.net>
2016-09-23 08:14:17 +0200

    feature: fr - update validation.uploaded

--- src/fr/validation.php
+++ src/fr/validation.php
@@ -81,7 +81,7 @@ return [
     'string'               => 'Le champ :attribute doit être une chaîne de caractères.',
     'timezone'             => 'Le champ :attribute doit être un fuseau horaire valide.',
     'unique'               => 'La valeur du champ :attribute est déjà utilisée.',
-    'uploaded'             => 'The :attribute failed to upload.',
+    'uploaded'             => "Le fichier du champ :attribute n'a pu être téléchargé.",
     'url'                  => "Le format de l'URL de :attribute n'est pas valide.",
 
     /*

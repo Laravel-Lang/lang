47c23ddad800d799080ab950ae2fbfaef143111b
karim khan <kkhancse91@gmail.com>
2016-10-09 14:02:37 +0600

    update passwords.php file and add bangla language

--- src/bn/passwords.php
+++ src/bn/passwords.php
@@ -13,10 +13,10 @@ return [
     |
     */
 
-    'password' => 'Passwords must be at least six characters and match the confirmation.',
-    'reset'    => 'Your password has been reset!',
-    'sent'     => 'We have e-mailed your password reset link!',
-    'token'    => 'This password reset token is invalid.',
-    'user'     => "We can't find a user with that e-mail address.",
+    'password' => 'পাসওয়ার্ডে কমপক্ষে ছয় অক্ষরের হতে হবে এবং নিশ্চিতকরণ মেলাতে হবে .',
+    'reset'    => 'আপনার পাসওয়ার্ড পুনরায় সেট করা হয়েছে!',
+    'sent'     => 'আমরা আপনার পাসওয়ার্ড পুনরায় সেট করার লিঙ্ক ইমেইল করেছি !',
+    'token'    => 'এই পাসওয়ার্ড রিসেট টোকেন অবৈধ.',
+    'user'     => "আমরা এই  ইমেল ঠিকানা দিয়ে  ব্যবহারকারী খুঁজে পাচ্ছি না",
 
 ];

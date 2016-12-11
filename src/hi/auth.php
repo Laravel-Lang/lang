5d680f3f29aed4e85c18c40694ae747013af3356
Igor Ilic <iggor.ilic@gmail.com>
2016-12-11 15:35:44 +0100

    Add files via upload

---
+++ src/hi/auth.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'failed' => 'ये साख हमारे रिकॉर्ड से मेल नहीं खा रहे हैं।',
+    'throttle' => 'बहुत सारे लॉगिन प्रयास। :seconds सेकंड में फिर से कोशिश करें।',
+
+];

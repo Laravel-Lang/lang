4f90981263395d0b58b54faf80ff242cc9bea654
doanh <pham.van.doanh@framgia.com>
2017-06-09 14:27:26 +0700

    [vi] Add missing translations

--- src/vi/passwords.php
+++ src/vi/passwords.php
@@ -16,7 +16,7 @@ return [
     'password' => 'Mật khẩu phải gồm 6 ký tự và khớp với phần xác nhận.',
     'reset'    => 'Mật khẩu mới đã được cập nhật!',
     'sent'     => 'Hướng dẫn cấp lại mật khẩu đã được gửi!',
-    'token'    => 'Mã cấp lại mật khẩu không hợp lệ.',
+    'token'    => 'Mã khôi phục mật khẩu không hợp lệ.',
     'user'     => 'Không tìm thấy người dùng với địa chỉ email này.',
 
 ];

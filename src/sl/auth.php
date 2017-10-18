e7d9b878c6f7cfd9a58cc93273dfbbf6a6b06a4f
Aleš Vaupotič <alesvaupotic@users.noreply.github.com>
2017-10-18 15:25:42 +0200

    Added a comma ...
    ... as in the original file.

--- src/sl/auth.php
+++ src/sl/auth.php
@@ -14,6 +14,6 @@ return [
     */
 
     'failed'   => 'Ti podatki se ne ujemajo z našimi.',
-    'throttle' => 'Preveč poskusov prijave. Prosimo, poskusite ponovno čez :seconds sekund.'
+    'throttle' => 'Preveč poskusov prijave. Prosimo, poskusite ponovno čez :seconds sekund.',
 
 ];

85b4acf1d5b14996b6b65b10b59e5906779b7fd4
fdelrieu <caouecs@caouecs.net>
2018-01-25 11:27:20 +0100

    feature: changelog file for next project

---
+++ changelog.php
@@ -0,0 +1,13 @@
+<?php
+return [
+    '2018-01-23' => [
+        'lv' => 'fix rename validation file',
+        'pl' => 'validation.ipv4 and validation.ipv6 updated',
+    ],
+    '2018-01-19' => [
+        'ug' => 'add new language : Uyghur',
+    ],
+    '2018-01-10' => [
+        'lv' => 'add new language : Latvia',
+    ],
+];

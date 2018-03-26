6adb1692d0cc0198f5323209425f2d7ff070645a
Andrey Helldar <helldar@ai-rus.com>
2018-03-26 12:35:52 +0300

    Translate "not_regex" to russian

--- src/ru/validation.php
+++ src/ru/validation.php
@@ -65,7 +65,7 @@ return [
         'array' => 'Количество элементов в поле :attribute должно быть не менее :min.',
     ],
     'not_in' => 'Выбранное значение для :attribute ошибочно.',
-    'not_regex' => 'The :attribute format is invalid.',
+    'not_regex' => 'Выбранный формат для :attribute ошибочный.',
     'numeric' => 'Поле :attribute должно быть числом.',
     'present' => 'Поле :attribute должно присутствовать.',
     'regex' => 'Поле :attribute имеет ошибочный формат.',

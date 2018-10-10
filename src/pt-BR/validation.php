978f3556063030333e8c5aee431b27102cdeca8b
Valter Oliveira <jr.valter.oliveira@gmail.com>
2018-10-10 11:58:01 +0200

    Translating not_regex value to pt-BR

--- src/pt-BR/validation.php
+++ src/pt-BR/validation.php
@@ -88,7 +88,7 @@ return [
         'array'   => 'O campo :attribute deve conter no mínimo :min itens.',
     ],
     'not_in'               => 'O campo :attribute contém um valor inválido.',
-    'not_regex'            => 'The :attribute format is invalid.',
+    'not_regex'            => 'O formato do valor :attribute é inválido.',
     'numeric'              => 'O campo :attribute deve conter um valor numérico.',
     'present'              => 'O campo :attribute deve estar presente.',
     'regex'                => 'O formato do valor informado no campo :attribute é inválido.',

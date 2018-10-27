a409fb96467f9d0f76115b56fb1fb53ff1320f60
Hicham LEMGHARI <cyberhicham@gmail.com>
2018-10-27 16:01:32 +0100

    Resolving merge conflict

--- src/fi/validation.php
+++ src/fi/validation.php
@@ -12,7 +12,6 @@ return [
     |
     */
 
-<<<<<<< HEAD
     'accepted' => 'Kenttä :attribute tulee hyväksyä.',
     'active_url' => 'Kentän :attribute tulee olla validi URL-osoite.',
     'after' => 'Kentän :attribute päiväyksen tulee olla :date jälkeen.',
@@ -24,25 +23,11 @@ return [
     'before' => 'Kentän :attribute päiväyksen tulee olla ennen :date.',
     'before_or_equal' => 'The :attribute must be a date before or equal to :date.',
     'between' => [
-=======
-    'accepted'             => 'Kenttä :attribute tulee hyväksyä.',
-    'active_url'           => 'Kentän :attribute tulee olla validi URL-osoite.',
-    'after'                => 'Kentän :attribute päiväyksen tulee olla :date jälkeen.',
-    'after_or_equal'       => 'Kentän :attribute päiväyksen tulee olla sama ja jälkeen :date',
-    'alpha'                => 'Kenttä :attribute voi sisältää vain kirjaimia.',
-    'alpha_dash'           => 'Kenttä :attribute voi sisältää vain kirjaimia, numeroita ja viivoja.',
-    'alpha_num'            => 'Kenttä :attribute voi sisältää vain kirjaimia ja numeroita.',
-    'array'                => 'Kenttä :attribute tulee olla taulukko.',
-    'before'               => 'Kentän :attribute päiväyksen tulee olla ennen :date.',
-    'before_or_equal'      => 'Kentän :attribute päiväyksen tulee olla ennen tai sama kuin :date.',
-    'between'              => [
->>>>>>> a81fae06722affcec435fa7f65655838f256548b
         'numeric' => 'Kentän :attribute tulee olla välillä :min - :max.',
         'file' => 'Tiedoston :attribute tulee olla :min - :max kilobittiä.',
         'string' => 'Kentän :attribute tulee olla :min - :max merkkiä pitkä.',
         'array' => 'Kentän :attribute tulee sisältää välillä :min - :max arvoa.',
     ],
-<<<<<<< HEAD
     'boolean' => 'Kentän :attribute arvon tulee olla tosi tai epätosi.',
     'confirmed' => 'Kentän :attribute vahvistus ei täsmää.',
     'date' => 'Kentän :attribute arvo ei ole kelvollinen päivämäärä.',
@@ -57,22 +42,6 @@ return [
     'file' => 'Kentän :attribute arvon tulee olla tiedosto.',
     'filled' => 'Kenttä :attribute on pakollinen.',
     'gt' => [
-=======
-    'boolean'              => 'Kentän :attribute arvon tulee olla tosi tai epätosi.',
-    'confirmed'            => 'Kentän :attribute vahvistus ei täsmää.',
-    'date'                 => 'Kentän :attribute arvo ei ole kelvollinen päivämäärä.',
-    'date_format'          => 'Kentän :attribute arvo ei vastaa muotoa :format.',
-    'different'            => 'Kenttien :attribute ja :other tulee olla eriarvoisia.',
-    'digits'               => 'Kentän :attribute arvon on oltava :digits numeroa.',
-    'digits_between'       => 'Kentän :attribute arvon tulee olla :min - :max numeroa.',
-    'dimensions'           => 'Kentän :attribute kuvalla on virheelliset mitat.',
-    'distinct'             => 'Kentän :attribute arvo ei ole uniikki.',
-    'email'                => 'Kentän :attribute arvo ei ole validi sähköpostiosoite.',
-    'exists'               => 'Kentän :attribute valittu arvo on virheellinen.',
-    'file'                 => 'Kentän :attribute arvon tulee olla tiedosto.',
-    'filled'               => 'Kenttä :attribute on pakollinen.',
-    'gt'                   => [
->>>>>>> a81fae06722affcec435fa7f65655838f256548b
         'numeric' => 'The :attribute must be greater than :value.',
         'file' => 'The :attribute must be greater than :value kilobytes.',
         'string' => 'The :attribute must be greater than :value characters.',
@@ -84,7 +53,6 @@ return [
         'string' => 'The :attribute must be greater than or equal :value characters.',
         'array' => 'The :attribute must have :value items or more.',
     ],
-<<<<<<< HEAD
     'image' => 'Kentän :attribute arvon tulee olla kuva.',
     'in' => 'Kentän :attribute arvo on virheellinen.',
     'in_array' => 'Kentän :attribute arvo ei sisälly kentän :other arvoon.',
@@ -94,17 +62,6 @@ return [
     'ipv6' => 'The :attribute must be a valid IPv6 address.',
     'json' => 'Kentän :attribute arvon tulee olla validia JSON:ia.',
     'lt' => [
-=======
-    'image'                => 'Kentän :attribute arvon tulee olla kuva.',
-    'in'                   => 'Kentän :attribute arvo on virheellinen.',
-    'in_array'             => 'Kentän :attribute arvo ei sisälly kentän :other arvoon.',
-    'integer'              => 'Kentän :attribute arvon tulee olla numero.',
-    'ip'                   => 'Kentän :attribute arvon tulee olla validi IP-osoite.',
-    'ipv4'                 => 'Kentän :attribute arvon tulee olla validi IPv4-osoite.',
-    'ipv6'                 => 'Kentän :attribute arvon tulee olla validi IPv6-osoite.',
-    'json'                 => 'Kentän :attribute arvon tulee olla validia JSON:ia.',
-    'lt'                   => [
->>>>>>> a81fae06722affcec435fa7f65655838f256548b
         'numeric' => 'The :attribute must be less than :value.',
         'file' => 'The :attribute must be less than :value kilobytes.',
         'string' => 'The :attribute must be less than :value characters.',
@@ -130,7 +87,6 @@ return [
         'string' => 'Kentän :attribute arvon tulee olla vähintään :min merkkiä.',
         'array' => 'Kentän :attribute tulee sisältää vähintään :min arvoa.',
     ],
-<<<<<<< HEAD
     'not_in' => 'Kentän :attribute arvo on virheellinen.',
     'not_regex' => 'The :attribute format is invalid.',
     'numeric' => 'Kentän :attribute arvon tulee olla numero.',
@@ -142,19 +98,6 @@ return [
     'required_with' => 'Kenttä :attribute vaaditaan kun arvo :values on annettu.',
     'required_with_all' => 'Kenttä :attribute vaaditaan kun arvo :values on annettu.',
     'required_without' => 'Kenttä :attribute vaaditaan kun arvoa :values ei ole annettu.',
-=======
-    'not_in'               => 'Kentän :attribute arvo on virheellinen.',
-    'not_regex'            => 'Kentän :attribute formaatti on virheellinen.',
-    'numeric'              => 'Kentän :attribute arvon tulee olla numero.',
-    'present'              => 'Kenttä :attribute vaaditaan.',
-    'regex'                => 'Kentän :attribute arvo on väärää muotoa.',
-    'required'             => 'Kenttä :attribute vaaditaan.',
-    'required_if'          => 'Kenttä :attribute vaaditaan kun :other on :value.',
-    'required_unless'      => 'Kenttä :attribute vaaditaan jos :other ei sisälly arvoihin :values.',
-    'required_with'        => 'Kenttä :attribute vaaditaan kun arvo :values on annettu.',
-    'required_with_all'    => 'Kenttä :attribute vaaditaan kun arvo :values on annettu.',
-    'required_without'     => 'Kenttä :attribute vaaditaan kun arvoa :values ei ole annettu.',
->>>>>>> a81fae06722affcec435fa7f65655838f256548b
     'required_without_all' => 'Kenttä :attribute vaaditaan kun mitään arvoista :values ei ole annettu.',
     'same' => 'Kenttien :attribute ja :other on oltava samanarvoiset.',
     'size' => [

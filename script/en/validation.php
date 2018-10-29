e7200eaef1de52589fbc0e301693d403aa22ad7c
Fred Delrieu <caouecs@users.noreply.github.com>
2018-10-29 07:59:30 +0100

    feature: add validation.uuid
    See https://github.com/laravel/laravel/pull/4834

--- script/en/validation.php
+++ script/en/validation.php
@@ -111,6 +111,7 @@ return [
     'unique'               => 'The :attribute has already been taken.',
     'uploaded'             => 'The :attribute failed to upload.',
     'url'                  => 'The :attribute format is invalid.',
+    'uuid'                 => 'The :attribute must be a valid UUID.',
 
     /*
     |--------------------------------------------------------------------------

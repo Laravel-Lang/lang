651c2621aa930e4c22ffd8322a40d3f6fac528f5
Asier Iturralde Sarasola <asier.iturralde@gmail.com>
2018-10-31 19:55:44 +0100

    [eu] Update Basque translation

--- src/eu/passwords.php
+++ src/eu/passwords.php
@@ -12,9 +12,9 @@ return [
     |
     */
 
-    'password' => 'Pasahitzak gutxienez sei karaktere euki behar dituzte eta bat etorri behar dira.',
-    'reset'    => 'Zure pasahitza berrezarritua izan da!',
+    'password' => 'Pasahitzak gutxienez sei karaktere eduki behar dituzte eta bat etorri behar dira.',
+    'reset'    => 'Zure pasahitza berrezarri da!',
     'sent'     => 'Zure pasahitza berrezartzeko esteka postaz bidali dizugu!',
     'token'    => 'Pasahitza berreskuratzeko tokena baliogabea da.',
-    'user'     => 'Ezin izan dugu posta helbide horrekin bat datorren erabiltzailerik aurkitu.',
+    'user'     => 'Ezin izan dugu helbide elektroniko horrekin bat datorren erabiltzailerik aurkitu.',
 ];

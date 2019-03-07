065710b9bd56172447b7382a34917b986965886e
caouecs <caouecs@caouecs.net>
2019-03-07 19:18:01 +0100

    feature: fr - passwords.password

--- src/fr/passwords.php
+++ src/fr/passwords.php
@@ -12,7 +12,7 @@ return [
     |
     */
 
-    'password' => 'Les mots de passe doivent contenir au moins six caractères et être identiques.',
+    'password' => 'Les mots de passe doivent contenir au moins huit caractères et être identiques.',
     'reset'    => 'Votre mot de passe a été réinitialisé !',
     'sent'     => 'Nous vous avons envoyé par email le lien de réinitialisation du mot de passe !',
     'token'    => "Ce jeton de réinitialisation du mot de passe n'est pas valide.",

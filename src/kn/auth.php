f8a2211cae56595acf8b11c376bb33d1898e098a
Raghavendra <raghav.php@gmail.com>
2019-03-08 13:57:02 +0530

    [kn] Kannada New Language

---
+++ src/kn/auth.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'failed' => 'ಈ ರುಜುವಾತುಗಳು ನಮ್ಮ ದಾಖಲೆಗಳಿಗೆ ಹೊಂದಿಕೆಯಾಗುವುದಿಲ್ಲ.',
+    'throttle' => 'ಹಲವಾರು ಲಾಗಿನ್ ಪ್ರಯತ್ನಗಳು. ದಯವಿಟ್ಟು ಮತ್ತೆ ಪ್ರಯತ್ನಿಸಿ :seconds	 ಸೆಕೆಂಡುಗಳಲ್ಲಿ',
+];

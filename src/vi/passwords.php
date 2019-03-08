d02bd34701e3a68efae263d654ec2314188df4c9
Dinh Quoc Han <contact@dinhquochan.com>
2019-03-08 08:40:37 +0700

    feature: vi - passwords.password

--- src/vi/passwords.php
+++ src/vi/passwords.php
@@ -12,7 +12,7 @@ return [
     |
     */
 
-    'password' => 'Mật khẩu phải gồm 6 ký tự và khớp với phần xác nhận.',
+    'password' => 'Mật khẩu phải gồm 8 ký tự và khớp với phần xác nhận.',
     'reset'    => 'Mật khẩu mới đã được cập nhật!',
     'sent'     => 'Hướng dẫn cấp lại mật khẩu đã được gửi!',
     'token'    => 'Mã khôi phục mật khẩu không hợp lệ.',

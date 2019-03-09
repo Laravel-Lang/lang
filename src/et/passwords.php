5414629a517c531913589ae81d418335f060d79f
caouecs <caouecs@caouecs.net>
2019-03-09 11:53:46 +0100

    feature: laravel 5.8 password : 6 to 8

--- src/et/passwords.php
+++ src/et/passwords.php
@@ -12,7 +12,7 @@ return [
     |
     */
 
-    'password' => 'Parool peab olema vähemalt 6 tähemärki pikk ja vastama kinnitusele.',
+    'password' => 'Parool peab olema vähemalt 8 tähemärki pikk ja vastama kinnitusele.',
     'reset'    => 'Sinu parool on lähtestatud!',
     'sent'     => 'Oleme saatnud sulle e-postiga parooli lähtestamise lingi!',
     'token'    => 'See parooli lähtestamise tunnus on vigane.',
--- src/lv/passwords.php
+++ src/lv/passwords.php
@@ -12,7 +12,7 @@ return [
     |
     */
 
-    'password' => 'Parolei jābūt vismaz 6 zīmes garai un jāatbilst apstiprinājumam.',
+    'password' => 'Parolei jābūt vismaz 8 zīmes garai un jāatbilst apstiprinājumam.',
     'reset'    => 'Jūsu parole ir atjaunināta!',
     'sent'     => 'Mēs nosūtījām paroles maiņas linku uz jūsu e-pastu!',
     'token'    => 'Šāda zīme pie paroles maiņas nav atļauta.',

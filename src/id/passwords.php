6b65b3f7d81337fdef203be7196f886567b796b6
Ian Mustafa <cuplizian@gmail.com>
2019-03-15 21:47:29 +0700

    [5.8] Bahasa Indonesia: Update passwords.php

--- src/id/passwords.php
+++ src/id/passwords.php
@@ -12,7 +12,7 @@ return [
     |
     */
 
-    'password' => 'Kata sandi minimal harus memiliki enam karakter dan cocok dengan konfirmasi.',
+    'password' => 'Kata sandi minimal harus memiliki delapan karakter dan cocok dengan konfirmasi.',
     'reset'    => 'Kata sandi Anda sudah direset!',
     'sent'     => 'Kami sudah mengirim surel yang berisi tautan untuk mereset kata sandi Anda!',
     'token'    => 'Token pengaturan ulang kata sandi tidak sah.',

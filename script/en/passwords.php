83cd05e6f73f6b9eee8e6e05d984811bed3b3015
Pawel Mysior <pawelmysior@gmail.com>
2019-03-20 13:12:36 +0100

    Fix style to match the original files

--- script/en/passwords.php
+++ script/en/passwords.php
@@ -1,6 +1,7 @@
 <?php
 
 return [
+
     /*
     |--------------------------------------------------------------------------
     | Password Reset Language Lines
@@ -12,9 +13,10 @@ return [
     |
     */
 
-    'reset'    => 'Your password has been reset!',
-    'sent'     => 'We have e-mailed your password reset link!',
-    'token'    => 'This password reset token is invalid.',
-    'user'     => "We can't find a user with that e-mail address.",
     'password' => 'Passwords must be at least eight characters and match the confirmation.',
+    'reset' => 'Your password has been reset!',
+    'sent' => 'We have e-mailed your password reset link!',
+    'token' => 'This password reset token is invalid.',
+    'user' => "We can't find a user with that e-mail address.",
+
 ];

f8762c0692608ef9ad966cfaf591101bc400e047
gvete <girish.vete@sonorasoftware.com>
2019-07-04 12:14:33 +0530

    Added Marathi (MR) language.

---
+++ src/mr/passwords.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'password' => 'पासवर्ड मध्ये कमीत कमी सहा वर्ण अपेक्षित आहेत आणि ते नियमामध्ये बसावेत.',
+    'reset'    => 'आपला पासवर्ड पुनर्रचीत केला गेला आहे.',
+    'sent'     => 'आपल्याला पासवर्ड पुनर्रचीत करायचा मेल केला गेला आहे.',
+    'token'    => 'आपला पासवर्ड पुनर्रचीत करायचे टोकं अवैध नाही.',
+    'user'     => 'या ई-मेल वर कोणतेही खाते उपलब्ध नाही.',
+];

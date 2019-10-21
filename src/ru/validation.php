629473846f0be1bcc53d597c1b9e80a1ec71fadf
Andrey Helldar <helldar@ai-rus.com>
2019-10-21 10:02:51 +0300

    [ru] Grammar inaccuracy fixed (not a bug)

--- src/ru/validation.php
+++ src/ru/validation.php
@@ -93,7 +93,7 @@ return [
     'not_in'               => 'Выбранное значение для :attribute ошибочно.',
     'not_regex'            => 'Выбранный формат для :attribute ошибочный.',
     'numeric'              => 'Поле :attribute должно быть числом.',
-    'password'             => 'Не верный пароль.',
+    'password'             => 'Неверный пароль.',
     'present'              => 'Поле :attribute должно присутствовать.',
     'regex'                => 'Поле :attribute имеет ошибочный формат.',
     'required'             => 'Поле :attribute обязательно для заполнения.',

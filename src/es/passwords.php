a1b753daeb451c9e418cfa00f2ba5b7d559549d7
JuanDMeGon <JuanDMeGon@users.noreply.github.com>
2019-10-27 20:03:48 -0500

    This field is not used anymore on Laravel 6.

--- src/es/passwords.php
+++ src/es/passwords.php
@@ -12,7 +12,6 @@ return [
     |
     */
 
-    'password' => 'Las contraseñas deben coincidir y contener al menos 8 caracteres',
     'reset'    => '¡Tu contraseña ha sido restablecida!',
     'sent'     => '¡Te hemos enviado por correo el enlace para restablecer tu contraseña!',
     'token'    => 'El token de recuperación de contraseña es inválido.',

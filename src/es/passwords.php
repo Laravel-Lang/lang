66dd9c1ac0cfe7d76a4296d399164910e591d3d1
JuanDMeGon <JuanDMeGon@users.noreply.github.com>
2019-10-27 19:54:12 -0500

    Adding new field
    Adding a new validation field created for Laravel 6.

--- src/es/passwords.php
+++ src/es/passwords.php
@@ -12,9 +12,10 @@ return [
     |
     */
 
-    'password' => 'Las contraseñas deben coincidir y contener al menos 8 caracteres',
-    'reset'    => '¡Tu contraseña ha sido restablecida!',
-    'sent'     => '¡Te hemos enviado por correo el enlace para restablecer tu contraseña!',
-    'token'    => 'El token de recuperación de contraseña es inválido.',
-    'user'     => 'No podemos encontrar ningún usuario con ese correo electrónico.',
+    'password'  => 'Las contraseñas deben coincidir y contener al menos 8 caracteres',
+    'reset'     => '¡Tu contraseña ha sido restablecida!',
+    'sent'      => '¡Te hemos enviado por correo el enlace para restablecer tu contraseña!',
+    'token'     => 'El token de recuperación de contraseña es inválido.',
+    'user'      => 'No podemos encontrar ningún usuario con ese correo electrónico.',
+    'throttled' => 'Por favor espera antes de intentar de nuevo.',
 ];

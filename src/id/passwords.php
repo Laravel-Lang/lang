632da22bd51006504fabd4c880f8cbb8be100e21
David Adi Nugroho <davidadi216@gmail.com>
2019-11-04 13:28:54 +0700

    [id] Add missing id translations

--- src/id/passwords.php
+++ src/id/passwords.php
@@ -15,7 +15,7 @@ return [
     'password'  => 'Kata sandi minimal harus memiliki enam karakter dan cocok dengan konfirmasi.',
     'reset'     => 'Kata sandi Anda sudah direset!',
     'sent'      => 'Kami sudah mengirim surel yang berisi tautan untuk mereset kata sandi Anda!',
-    'throttled' => 'Please wait before retrying.',
+    'throttled' => 'Harap tunggu sebelum mencoba lagi.',
     'token'     => 'Token pengaturan ulang kata sandi tidak sah.',
     'user'      => 'Kami tidak dapat menemukan pengguna dengan alamat surel tersebut.',
 ];

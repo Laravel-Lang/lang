3d18a76da5857869910709526101568fd695e8f5
Made I.T <info@madeit.be>
2019-11-05 23:33:42 +0100

    Update passwords.php

--- src/nl/passwords.php
+++ src/nl/passwords.php
@@ -15,7 +15,7 @@ return [
     'password'  => 'Wachtwoord moet minimaal acht tekens lang zijn en de wachtwoorden moeten overeenkomen.',
     'reset'     => 'Het wachtwoord van uw account is gewijzigd.',
     'sent'      => 'We hebben een e-mail verstuurd met instructies om een nieuw wachtwoord in te stellen.',
-    'throttled' => 'Please wait before retrying.',
+    'throttled' => 'Gelieve even te wachten voor u het opnieuw probeert.',
     'token'     => 'Dit wachtwoordhersteltoken is niet geldig.',
     'user'      => 'Geen gebruiker bekend met het e-mailadres.',
 ];

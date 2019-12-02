43867b8887dc38cbc03602cbc15f051e8e90b75a
Tahsin Gökalp Şaan <tahsinsaan@gmail.com>
2019-12-02 23:02:49 +0300

    [tr] Updated passwords.php

--- src/tr/passwords.php
+++ src/tr/passwords.php
@@ -15,7 +15,7 @@ return [
     'password'  => 'Parolanız en az altı karakter olmalı ve doğrulama ile eşleşmelidir.',
     'reset'     => 'Parolanız sıfırlandı!',
     'sent'      => 'Parola sıfırlama bağlantınız e-posta ile gönderildi!',
-    'throttled' => 'Please wait before retrying.',
+    'throttled' => 'Lütfen tekrar denemeden önce bekleyiniz.',
     'token'     => 'Parola sıfırlama adresi/kodu geçersiz.',
     'user'      => 'Bu e-posta adresi ile kayıtlı bir üye bulunmuyor.',
 ];

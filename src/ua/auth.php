8d47173290f2570ec949fba8faeccfce7a661776
rela589n <zsidelnik@gmail.com>
2020-01-19 21:02:55 +0200

    Auth

---
+++ src/ua/auth.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'failed'   => 'Ім\'я користувача і(або) пароль не збігаються.',
+    'throttle' => 'Занадто багато спроб входу. Повторіть ще раз через :seconds секунд.',
+];

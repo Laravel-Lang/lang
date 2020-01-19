75cccd547930631f894f2416a403a7467356fe53
rela589n <zsidelnik@gmail.com>
2020-01-19 21:10:37 +0200

    Passwords

---
+++ src/ua/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'reset'     => 'Ваш пароль успішно скинуто!',
+    'sent'      => 'Ми надіслали посилання для відновлення пароля на електронну пошту!',
+    'throttled' => 'Перш ніж повторити спробу, будь ласка, зачекайте.',
+    'token'     => 'Цей код скидання пароля є недійсний.',
+    'user'      => 'Ми не можемо знайти користувача з цією електронною адресою.',
+
+];

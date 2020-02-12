39bf18bdb018608d1458730cfaea762e7f96241a
Monur <monurakkaya@gmail.com>
2020-02-12 12:58:30 +0300

    added custom attributes and attributes explanations

--- src/tr/validation.php
+++ src/tr/validation.php
@@ -118,32 +118,39 @@ return [
 
     /*
     |--------------------------------------------------------------------------
-    | Custom Validation Language Lines
+    | Özelleştirilmiş doğrulama mesajları
     |--------------------------------------------------------------------------
     |
-    | Here you may specify custom validation messages for attributes using the
-    | convention "attribute.rule" to name the lines. This makes it quick to
-    | specify a specific custom language line for a given attribute rule.
+    | Bu alanda her niteleyici (attribute) ve kural (rule) ikilisine özel hata
+    | mesajları tanımlayabilirsiniz. Bu özellik, son kullanıcıya daha gerçekçi
+    | metinler göstermeniz için oldukça faydalıdır.
+    |
+    | Örnek olarak:
+    |
+    | 'email.email': 'Girdiğiniz e-posta adresi geçerli değil.'
+    | 'x.regex': 'x alanı için "a-b.c" formatında veri girmelisiniz.'
     |
     */
-
     'custom' => [
-        'attribute-name' => [
-            'rule-name' => 'custom-message',
+        'niteleyici-adi' => [
+            'kural-adi' => 'ozel-hata-mesaji',
         ],
     ],
 
     /*
     |--------------------------------------------------------------------------
-    | Custom Validation Attributes
+    | Özelleştirilmiş niteleyici isimleri
     |--------------------------------------------------------------------------
     |
-    | The following language lines are used to swap attribute place-holders
-    | with something more reader friendly such as E-Mail Address instead
-    | of "email". This simply helps us make messages a little cleaner.
+    | Bu alandaki bilgiler "email" gibi niteleyici isimlerini "e-posta adresi"
+    | gibi daha okunabilir metinlere çevirmek için kullanılır. Bu bilgiler
+    | hata mesajlarının daha temiz olmasını sağlar.
+    |
+    | Örnek olarak:
+    |
+    | 'email' => 'e-posta adresi',
+    | 'password' => 'parola',
     |
     */
-
-    'attributes' => [
-    ],
+    'attributes' => [],
 ];

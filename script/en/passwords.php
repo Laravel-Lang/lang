db23833785bb4e675ed1d06582f375ccbb8d4e6a
caouecs <caouecs@caouecs.net>
2020-03-01 15:11:14 +0100

    feature: passwords.password not needed for Laravel 6

--- script/en/passwords.php
+++ script/en/passwords.php
@@ -12,7 +12,6 @@ return [
     |
     */
 
-    'password'  => 'Passwords must be at least eight characters and match the confirmation.',
     'reset'     => 'Your password has been reset!',
     'sent'      => 'We have emailed your password reset link!',
     'throttled' => 'Please wait before retrying.',

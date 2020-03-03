b42dd97ec7a3be06b90dc926228c111b03c2bb1c
Knarik <knarik.yeritsyan@gmail.com>
2020-03-03 16:45:26 +0400

    Added Armenian(hy) language

---
+++ src/hy/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'reset' => 'Ձեր գաղտնաբառը վերականգնվել է',
+    'sent' => 'Ձեր գաղտնաբառի վերականգնման հղումն ուղարկվել է ձեր Էլ․ հասցեին',
+    'throttled' => 'Խնդրում ենք սպասել նախքան կրկին փորձելը։',
+    'token' => 'Գաղտնաբառի վերականգնման կոդը սխալ է։',
+    'user' => 'Տվյալ Էլ․ հասցեով օգտատեր չի գտնվել։',
+
+];

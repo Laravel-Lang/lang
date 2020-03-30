b55f0a8338bec5f9ade55b3b4c866777cd5f335b
Ogün Karakuş <kirk5bucuk@gmail.com>
2020-03-30 12:57:22 +0300

    Indent fixes
    Signed-off-by: Ogün Karakuş <kirk5bucuk@gmail.com>

--- src/tr/passwords.php
+++ src/tr/passwords.php
@@ -13,10 +13,10 @@ return [
     |
     */
 
-    'reset' => 'Parolanız sıfırlandı!',
-    'sent' => 'Parola sıfırlama bağlantınız e-posta ile gönderildi!',
+    'reset'     => 'Parolanız sıfırlandı!',
+    'sent'      => 'Parola sıfırlama bağlantınız e-posta ile gönderildi!',
     'throttled' => 'Lütfen tekrar denemeden önce bekleyiniz.',
-    'token' => 'Parola sıfırlama adresi/kodu geçersiz.',
-    'user' => 'Bu e-posta adresi ile kayıtlı bir üye bulunmuyor.',
+    'token'     => 'Parola sıfırlama adresi/kodu geçersiz.',
+    'user'      => 'Bu e-posta adresi ile kayıtlı bir üye bulunmuyor.',
 
 ];

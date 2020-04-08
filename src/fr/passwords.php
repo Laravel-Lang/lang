7077332a4c19e3a06231eb75d8e777477f78a3c6
kesm0 <hugolovighi@gmail.com>
2020-04-08 16:50:45 +0200

    Update french passwords.throttled
    Better translation ! Previous wasn't really correct :)

--- src/fr/passwords.php
+++ src/fr/passwords.php
@@ -14,7 +14,7 @@ return [
 
     'reset'     => 'Votre mot de passe a été réinitialisé !',
     'sent'      => 'Nous vous avons envoyé par email le lien de réinitialisation du mot de passe !',
-    'throttled' => 'Veuillez attendre afin de ré-essayer.',
+    'throttled' => 'Veuillez patienter avant de réessayer.',
     'token'     => "Ce jeton de réinitialisation du mot de passe n'est pas valide.",
     'user'      => "Aucun utilisateur n'a été trouvé avec cette adresse email.",
 ];

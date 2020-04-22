a1c5a7a339bcc881448ccfd49b0f4b3265960c82
Gerard Nesta <gerardnll@users.noreply.github.com>
2020-04-22 19:06:24 +0200

    Update passwords.php

--- src/ca/passwords.php
+++ src/ca/passwords.php
@@ -13,8 +13,8 @@ return [
     */
 
     'reset'     => 'La contrasenya s\'ha restablert!',
-    'sent'      => 'T\'hem enviat per e-mail un enllaç per a reiniciar la teva contrasenya!',
-    'throttled' => 'Please wait before retrying.',
+    'sent'      => 'Li hem enviat un correu electrònic amb un enllaç per a reiniciar la teva contrasenya!',
+    'throttled' => 'Si us plau, esperi abans de tornar-ho a intentar.',
     'token'     => 'Aquest token de recuperació de contrasenya és invàlid.',
     'user'      => 'No existeix cap usuari amb aquest correu.',
 ];

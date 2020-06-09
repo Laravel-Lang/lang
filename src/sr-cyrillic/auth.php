2aab20ef9096e59cfd5af2d14ce99e5d7c728772
Luka Latkovic <latkovic.luka@gmail.com>
2020-06-09 22:34:24 +0200

    Added [sr- cyrillic], Renamed [sr] to [sr-latin]

---
+++ src/sr-cyrillic/auth.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'failed'   => 'Подаци не одговарају ни једном налогу.',
+    'throttle' => 'Превише неуспелих покушаја. Покушајте поново за :seconds секунди.',
+];

2a92c6490a5fcff96f1addc1468a568d5de061fe
Lasse <post@lasseee.com>
2020-06-21 09:10:35 +0200

    Improve translations

--- src/nb/pagination.php
+++ src/nb/pagination.php
@@ -1,6 +1,7 @@
 <?php
 
 return [
+
     /*
     |--------------------------------------------------------------------------
     | Pagination Language Lines
@@ -13,5 +14,6 @@ return [
     */
 
     'previous' => '&laquo; Forrige',
-    'next'     => 'Neste &raquo;',
+    'next' => 'Neste &raquo;',
+
 ];

f399b7bab0582f544f98afceade06ced158998ba
Dbase Chhetri <66884874+dbasechhetri@users.noreply.github.com>
2020-08-06 01:47:42 +0545

    updated
    more words added

--- src/ne/passwords.php
+++ src/ne/passwords.php
@@ -12,9 +12,25 @@ return [
     |
     */
 
+    'Laravel'   => 'लाराभेल',
     'reset'     => 'पासवर्ड रिसेट भयो!',
+    'Dashboard' => 'ड्यासबोर्ड',
     'sent'      => 'हामीले पासवर्ड परिवर्तन गर्ने लिंक ईमेलमा पठाएका छौ!',
-    'throttled' => 'Please wait before retrying.',
+    'throttled' => 'पुन: प्रयास गर्नु अघि कृपया प्रतीक्षा गर्नुहोस्.',
     'token'     => 'यो पासवर्ड रिसेट गर्ने टोकन अमान्य छ।',
     'user'      => 'दिइएको ईमेल प्रयोगकर्ता भेटिएन।',
+    'Password'  => 'पासवर्ड',
+    'Reset Password'=> 'पासवर्ड रिसेट',
+    'E-Mail Address'=> 'इमेल ठेगाना',
+    'Confirm Password'=> 'पासवर्ड सुनिश्चित गर्नुहोस',
+    'Remember Me'=> 'मलाई सम्झनुहोस्',
+    'You are logged in'=> 'तपाई लगईन हुनुहुन्छ',
+    'Forgot Your Password?'=> 'तपाईको पासवर्ड बिर्सनुभयो?',
+    'Send Password Reset Link'=> 'पासवर्ड रिसेट लिंक पठाउनुहोस्',
+    'If you did not receive the email'=> 'यदि तपाईंले ईमेल प्राप्त गर्नुभएको छैन भने',
+    'click here to request another'=> 'अर्को अनुरोध गर्न यहाँ क्लिक गर्नुहोस्',
+    'Verify Your Email Address'=> 'तपाईको इमेल ठेगाना प्रमाणित गर्नुहोस्',
+    'A fresh verification link has been sent to your email address'=> 'नयाँ ईमेल प्रमाणिकरण लिंक तपाईको इ-मेल ठेगानामा पठाइएको छ',
+    'Before proceeding, please check your email for a verification link'=> 'अघि बढ्नु अघि कृपया तपाईंको ईमेल प्रमाणिकरण लिंकका लागि जाँच गर्नुहोस्',
+    'Please confirm your password before continuing'=> 'कृपया जारी राख्नु अघि तपाईंको पासवर्ड पुष्टि गर्नुहोस्',
 ];

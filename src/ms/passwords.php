a9a4460eacde96e57e2e9aa2e2a6b54491b58bf9
Ashraf Kamarudin <ashrafk@runcloud.io>
2020-10-04 17:10:35 +0800

    translate throttled to bahasa

--- src/ms/passwords.php
+++ src/ms/passwords.php
@@ -14,7 +14,7 @@ return [
 
     'reset'     => 'Kata laluan anda telah ditetapkan semula!',
     'sent'      => 'Kami telah e-mel pautan set semula kata laluan anda!',
-    'throttled' => 'Please wait before retrying.',
+    'throttled' => 'Sila tunggu sebentar sebelum mencuba semula.',
     'token'     => 'Token set semula kata laluan ini tidak sah.',
     'user'      => 'Kami tidak dapat mencari pengguna dengan alamat e-mel tersebut.',
 ];

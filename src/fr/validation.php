9ec011c7b4d951013c0fa448a170c298b9b77ebe
alancolant <alan.colant@keoconcept.com>
2020-10-09 02:46:52 +0200

    Update validation.php
    add current_pass_field for jetstream

--- src/fr/validation.php
+++ src/fr/validation.php
@@ -153,6 +153,7 @@ return [
         'last_name'             => 'nom',
         'password'              => 'mot de passe',
         'password_confirmation' => 'confirmation du mot de passe',
+        'current_password'      => 'mot de passe actuel',
         'city'                  => 'ville',
         'country'               => 'pays',
         'address'               => 'adresse',

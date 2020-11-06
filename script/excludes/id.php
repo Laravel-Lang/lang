b273b0b66c9d57443df535f07d47c5a15e3b07d1
mrnothing <41483685+Mammo32@users.noreply.github.com>
2020-11-06 21:52:56 +0700

    Update id.php

--- script/excludes/id.php
+++ script/excludes/id.php
@@ -1,1 +1,19 @@
-/**
+<?php
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Exclusion list
+    |--------------------------------------------------------------------------
+    |
+    | This is a list of exclusions for words or phrases where the original
+    | form of the word has the same spelling in a given language.
+    |
+    */
+
+    'Administrator',
+    'Admin',
+    'Password',
+    'Dashboard',
+    'Editor',
+];

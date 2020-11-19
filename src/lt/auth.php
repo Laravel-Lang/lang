b236a6561fd0a697f02a4adcd418660e587d411c
Marius Kasparavičius <marius.kasparavicius@mif.stud.vu.lt>
2020-11-19 18:22:58 +0200

    [lt] Translation of auth.password

--- src/lt/auth.php
+++ src/lt/auth.php
@@ -12,5 +12,6 @@ return [
     */
 
     'failed'   => 'Prisijungimo duomenys neatitinka.',
+    'password' => 'Pateiktas slaptažodis yra neteisingas.',
     'throttle' => 'Per daug bandymų prisijungti. Bandykite po :seconds sec.',
 ];

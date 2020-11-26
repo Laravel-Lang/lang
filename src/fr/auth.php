6948529e00bbca4d619aa1f3674bb1314a480a36
Fred Delrieu <caouecs@users.noreply.github.com>
2020-11-26 11:20:15 +0100

    [fr] update auth.php - add password

--- src/fr/auth.php
+++ src/fr/auth.php
@@ -13,5 +13,6 @@ return [
     */
 
     'failed'   => 'Ces identifiants ne correspondent pas à nos enregistrements',
+    'password' => 'Le mot de passe fourni est incorrect.',
     'throttle' => 'Tentatives de connexion trop nombreuses. Veuillez essayer de nouveau dans :seconds secondes.',
 ];

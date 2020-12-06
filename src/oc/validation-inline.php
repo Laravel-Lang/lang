7510e561b7c094a389f8d09de22424eb1281e4b8
Mejans <61360811+Mejans@users.noreply.github.com>
2020-12-06 17:41:46 +0100

    Update validation-inline.php

--- src/oc/validation-inline.php
+++ src/oc/validation-inline.php
@@ -1,7 +1,6 @@
 <?php
 
 return [
-
     /*
     |--------------------------------------------------------------------------
     | Validation Language Lines
@@ -30,43 +29,71 @@ return [
         'array'   => 'Lo tablèu :attribute deu aver entre :min e :max elements.',
     ],
     'boolean'              => 'Lo camp :attribute deu èsser true o false.',
-    'confirmed'            => 'La confirmacion :attribute correspond pas.',
-    'date'                 => 'La data :attribute es pas valida.',
+    'confirmed'            => 'Lo camp de confirmacion :attribute correspond pas.',
+    'date'                 => 'Lo camp :attribute es pas una data valida.',
+    'date_equals'          => 'Lo camp :attribute deu èsser una data egala a :date.',
     'date_format'          => ':attribute correspond pas al format :format.',
     'different'            => 'Los camps :attribute e :other devon èsser diferents.',
     'digits'               => ':attribute deu èsser un nombre de :digits chifras.',
     'digits_between'       => ':attribute deu èsser entre :min e :max chifras.',
-    'dimensions'           => ':attribute a de dimensions d’imatge invalidas.',
+    'dimensions'           => 'L’imatge :attribute a de dimensions invalidas.',
     'distinct'             => 'Lo camp :attribute a un doblon.',
     'email'                => ':attribute deu èsser una adreça de corrièl valida.',
+    'ends_with'            => 'Lo camp :attribute deu acabar per una de las valors seguentas : :values',
     'exists'               => 'Lo :attribute seleccionat es invalid.',
-    'file'                 => ':attribute deu èsser un fichièr.',
-    'filled'               => 'Lo camp:attribute deu aver una valor.',
-    'image'                => ':attribute deu èsser un imatge.',
-    'in'                   => 'Lo :attribute selecionnat es invalid.',
+    'file'                 => 'Lo camp :attribute deu èsser un fichièr.',
+    'filled'               => 'Lo camp :attribute deu aver una valor.',
+     'gt'                   => [
+        'numeric' => 'La valor de :attribute deu èsser superiora a :value.',
+        'file'    => 'La talha del fichièr de :attribute deu èsser superiora a :value kilo-octets.',
+        'string'  => 'Lo tèxt :attribute deu conténer mai de :value caractèrs.',
+        'array'   => 'Lo tablèu :attribute deu conténer mai de :value elements.',
+    ],
+    'gte'                  => [
+        'numeric' => 'La valor de :attribute deu èsser superiora o egala a :value.',
+        'file'    => 'La talha del fichièr de :attribute deu èsser superiora o egala a :value kilo-octets.',
+        'string'  => 'Lo tèxt :attribute deu conténer almens :value caractèrs.',
+        'array'   => 'Lo tablèu :attribute deu conténer almens :value elements.',
+    ],   
+    'image'                => 'Lo camp :attribute deu èsser un imatge.',
+    'in'                   => 'Lo camp :attribute selecionnat es invalid.',
     'in_array'             => 'Lo camp :attribute existís pas dins :other.',
     'integer'              => 'Lo camp :attribute deu èsser un nombre entièr.',
     'ip'                   => 'Lo camp :attribute deu èsser una adreça IP valida.',
     'ipv4'                 => 'Lo camp :attribute deu èsser una adreça IPv4 valida.',
     'ipv6'                 => 'Lo camp :attribute deu èsser una adreça IPv6 valida.',
     'json'                 => 'Lo camp :attribute deu èsser una cadena JSON valida.',
+    'lt'                   => [
+        'numeric' => 'La valor de :attribute deu èsser inferiora a :value.',
+        'file'    => 'La talha del fichièr de :attribute deu èsser inferiora a :value kilo-octets.',
+        'string'  => 'Lo tèxt :attribute deu conténer almens :value caractèrs.',
+        'array'   => 'Lo tablèu :attribute deu conténer almens :value elements.',
+    ],
+    'lte'                  => [
+        'numeric' => 'La valor de :attribute deu èsser inferiora o egala a :value.',
+        'file'    => 'La talha del fichièr de :attribute deu èsser inferiora o egala :value kilo-octets.',
+        'string'  => 'Lo tèxt :attribute deu conténer al pus mai :value caractèrs.',
+        'array'   => 'Lo tablèu :attribute deu conténer al pus mai :value elements.',
+    ],    
     'max'                  => [
-        'numeric' => ':attribute deu pas èsser superior a :max.',
-        'file'    => ':attribute deu pas èsser superior a :max kiloctets.',
-        'string'  => ':attribute deu èsser superior a :max caractèrs.',
-        'array'   => ':attribute deu pas conténer mai de :max elements.',
+        'numeric' => 'La valor de :attribute deu pas èsser superiora a :max.',
+        'file'    => 'La talha del fichièr:attribute deu pas èsser superior a :max kiloctets.',
+        'string'  => 'Lo tèxt :attribute deu èsser superior a :max caractèrs.',
+        'array'   => 'Lo tablèu :attribute deu pas conténer mai de :max elements.',
     ],
-    'mimes'                => ':attribute deu èsser un fichièr del tipe : :values.',
-    'mimetypes'            => ':attribute deu èsser un fichièr del tipe : :values.',
+    'mimes'                => 'Lo camp :attribute deu èsser un fichièr del tipe : :values.',
+    'mimetypes'            => 'Lo camp :attribute deu èsser un fichièr del tipe : :values.',
     'min'                  => [
-        'numeric' => ':attribute deu fa almens :min.',
-        'file'    => ':attribute deu fa almens :min kiloctets.',
-        'string'  => ':attribute deu fa almens :min caractèrs.',
-        'array'   => ':attribute deu conténer almens :min elements.',
+        'numeric' => 'La valor de :attribute deu fa almens :min o mai.',
+        'file'    => 'La talha del fichièr de :attribute deu fa almens :min kiloctets.',
+        'string'  => 'Lo tèxt :attribute deu fa almens :min caractèrs.',
+        'array'   => 'Lo tablèu :attribute deu conténer almens :min elements.',
     ],
-    'not_in'               => 'Lo :attribute seleccionat es invalid.',
+    'multiple_of'          => 'La valor de :attribute deu èsser un multiple de :value',    
+    'not_in'               => 'Lo camp :attribute seleccionat es invalid.',
     'not_regex'            => 'Lo format :attribute es invalid.',
-    'numeric'              => ':attribute deu èsser un nombre.',
+    'numeric'              => 'Lo camp :attribute deu èsser un nombre.',
+    'password'             => 'Lo senhal es incorrèct',
     'present'              => 'Lo camp :attribute deu èsser present.',
     'regex'                => 'Lo format :attribute es invalid.',
     'required'             => 'Lo camp :attribute es obligatòri.',
@@ -76,18 +103,20 @@ return [
     'required_with_all'    => 'Lo camp :attribute es obligatòri quand :values es present.',
     'required_without'     => 'Lo camp :attribute es obligatòri quand :values es pas present.',
     'required_without_all' => 'Lo camp :attribute es obligatòri quand cap de :values son presents.',
-    'same'                 => ':attribute e :other devon correspondre.',
+    'same'                 => 'Los camps :attribute e :other devon correspondre.',
     'size'                 => [
-        'numeric' => ':attribute deu fa :size.',
-        'file'    => ':attribute deu fa :size kiloctets.',
-        'string'  => ':attribute deu fa :size caractèrs.',
-        'array'   => ':attribute deu conténer :size elements.',
+        'numeric' => 'La valor de :attribute deu fa :size.',
+        'file'    => 'La talha del fichièr de :attribute deu fa :size kiloctets.',
+        'string'  => 'Lo tèxt :attribute deu fa :size caractèrs.',
+        'array'   => 'Lo tablèu :attribute deu conténer :size elements.',
     ],
-    'string'               => ':attribute deu èsser una cadena de tèxte.',
-    'timezone'             => ':attribute deu èsser una zòna valida.',
-    'unique'               => ':attribute es ja pres.',
-    'uploaded'             => ':attribute a pas pogut s’enviar.',
+    'starts_with'          => 'Lo camp :attribute deu començar amb una de las valors seguentas : :values',    
+    'string'               => 'Lo camp :attribute deu èsser una cadena de tèxt.',
+    'timezone'             => 'Lo camp :attribute deu èsser una zòna orària valida.',
+    'unique'               => 'La valor del camp :attribute es ja presa.',
+    'uploaded'             => 'Lo fichièr de :attribute a pas pogut s’enviar.',
     'url'                  => 'Lo format de :attribute es invalid.',
+    'uuid'                 => 'Lo camp :attribute deu èsser un UUID valid',
 
     /*
     |--------------------------------------------------------------------------
@@ -106,18 +135,6 @@ return [
         ],
     ],
 
-    /*
-    |--------------------------------------------------------------------------
-    | Custom Validation Attributes
-    |--------------------------------------------------------------------------
-    |
-    | The following language lines are used to swap attribute place-holders
-    | with something more reader friendly such as E-Mail Address instead
-    | of "email". This simply helps us make messages a little cleaner.
-    |
-    */
-
-    'attributes' => [],
     /*
     |--------------------------------------------------------------------------
     | Custom Validation Attributes

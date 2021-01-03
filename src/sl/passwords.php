f08a890427f773b7738c755542289edbdf0c82d4
harrallo <natalia.harrallo@gmail.com>
2021-01-03 14:09:17 +0100

    Update passwords.php

--- src/sl/passwords.php
+++ src/sl/passwords.php
@@ -14,7 +14,7 @@ return [
 
     'reset'     => 'Geslo je bilo spremenjeno!',
     'sent'      => 'Opomnik za geslo poslano!',
-    'throttled' => 'Please wait before retrying.',
+    'throttled' => 'Počakajte pred ponovnim poskusom.',
     'token'     => 'Ponastavitveni žeton je neveljaven.',
     'user'      => 'Ne moremo najti uporabnika s tem e-poštnim naslovom.',
 ];

6720568541e18273e67dfb3dbc2c8a8d1e56f3d1
Victor Moscoso <victorelec14@gmail.com>
2021-01-11 21:00:21 +0100

    German

--- src/de/auth.php
+++ src/de/auth.php
@@ -13,5 +13,6 @@ return [
     */
 
     'failed'   => 'Diese Kombination aus Zugangsdaten wurde nicht in unserer Datenbank gefunden.',
+    'password' => 'Das eingegebene Passwort ist nicht korrekt.',
     'throttle' => 'Zu viele Loginversuche. Versuchen Sie es bitte in :seconds Sekunden nochmal.',
 ];

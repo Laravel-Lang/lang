0555c77412412a5d2c7440bcf7a0060801aba22f
Jacques van Wyk <subs@jacquesvanwyk.com>
2021-01-15 13:58:26 +0200

    Added Afrikaans language support

---
+++ src/af/auth.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'failed' => 'Hierdie verwysings stem nie ooreen met ons rekords nie.',
+    'throttle' => 'Te veel pogings om aan te meld. Probeer asseblief weer binne :seconds sekondes',
+
+];

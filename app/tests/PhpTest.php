d0fb24c76d4ded9f952d6352a328a21c01e407bb
Andrey Helldar <helldar@ai-rus.com>
2021-04-08 22:48:07 +0300

    Removed hard linking to file names

--- app/tests/PhpTest.php
+++ app/tests/PhpTest.php
@@ -4,16 +4,16 @@ namespace Tests;
 
 use Helldar\PrettyArray\Services\File as Pretty;
 use Helldar\Support\Facades\Helpers\Filesystem\Directory;
+use Helldar\Support\Facades\Helpers\Filesystem\File;
+use Helldar\Support\Facades\Helpers\Str;
 
 final class PhpTest extends TestCase
 {
     protected string $target_path = __DIR__ . '/../../src';
 
-    protected array $files = ['auth.php', 'pagination.php', 'passwords.php', 'validation.php', 'validation-inline.php'];
-
     public function testPhp(): void
     {
-        foreach ($this->files as $filename) {
+        foreach ($this->files() as $filename) {
             $source = $this->source($filename);
 
             foreach ($this->locales() as $locale) {
@@ -28,6 +28,11 @@ final class PhpTest extends TestCase
                 }
             }
         }
+    }
+
+    protected function files(): array
+    {
+        return File::names($this->source_path, static fn ($filename) => Str::endsWith($filename, '.php'));
     }
 
     protected function locales(): array

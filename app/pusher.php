f4678cf081b7cd6bcb5f18e37dd4343c7008b6ff
Andrey Helldar <helldar@ai-rus.com>
2021-04-17 15:16:37 +0300

    [script] Added a script for creating commits separated by localizations

---
+++ app/pusher.php
@@ -0,0 +1,10 @@
+<?php
+
+use LaravelLang\Lang\Processors\Git;
+
+require __DIR__ . '/../vendor/autoload.php';
+
+/** @var \LaravelLang\Lang\Application $app */
+$app = require_once __DIR__ . '/bootstrap/app.php';
+
+$app->processor(Git::make());

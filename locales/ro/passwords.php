53f00b62f33526e2f3d1b9a47cdbf7fd0a8af24e
Andrey Helldar <helldar@ai-rus.com>
2021-04-17 15:14:14 +0300

    [ro] Machine translation of missing keys

--- locales/ro/passwords.php
+++ locales/ro/passwords.php
@@ -14,7 +14,7 @@
 return [
     'reset'     => 'Parola a fost resetată!',
     'sent'      => 'Am trimis un e-mail cu link-ul de resetare a parolei!',
-    'throttled' => 'Please wait before retrying.',
+    'throttled' => 'Vă rugăm să așteptați înainte de a încerca din nou.',
     'token'     => 'Codul de resetare a parolei este greșit.',
     'user'      => 'Nu există niciun utilizator cu această adresă de e-mail.',
 ];

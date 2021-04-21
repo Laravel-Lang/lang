bf9c7188d830db6637ca0157757f9880e5aa3105
caouecs <caouecs@caouecs.net>
2021-04-21 10:38:09 +0200

    [tr] exclude file

---
+++ excludes/tr.php
@@ -0,0 +1,66 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Exclusion list
+|--------------------------------------------------------------------------
+|
+| This is a list of exclusions for words or phrases where the original
+| form of the word has the same spelling in a given language.
+|
+*/
+
+return [
+    'Andorra',
+    'Angola',
+    'Anguilla',
+    'Aruba',
+    'Barbados',
+    'Belize',
+    'Benin',
+    'Bermuda',
+    'Burkina Faso',
+    'Burundi',
+    'Cape Verde',
+    'Curaçao',
+    'Fiji',
+    'Grenada',
+    'Guadeloupe',
+    'Guatemala',
+    'Guernsey',
+    'Guyana',
+    'Haiti',
+    'Honduras',
+    'Hong Kong',
+    'Jersey',
+    'Kenya',
+    'Lens',
+    'Lesotho',
+    'Libyan Arab Jamahiriya',
+    'Macao',
+    'Malta',
+    'Mauritius',
+    'Moldova',
+    'Montserrat',
+    'Myanmar',
+    'Nauru',
+    'Nepal',
+    'Niue',
+    'Pakistan',
+    'Panama',
+    'Paraguay',
+    'Peru',
+    'Qatar',
+    'Samoa',
+    'San Marino',
+    'Senegal',
+    'Sierra Leone',
+    'Sint Maarten (Dutch part)',
+    'Sri Lanka',
+    'Sudan',
+    'Timor-Leste',
+    'Uganda',
+    'Uruguay',
+    'Venezuela',
+    'Yemen',
+];

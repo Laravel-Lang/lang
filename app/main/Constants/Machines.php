ce9e1d5cfe3d8dfffb06e5c76386f2b7cd07dad1
Andrey Helldar <helldar@ai-rus.com>
2021-04-23 23:26:11 +0300

    Added the ability to flag machine translation

---
+++ app/main/Constants/Machines.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace LaravelLang\Lang\Constants;
+
+class Machines
+{
+    public const LOCALES = [
+        'az',
+        'be',
+        'bn',
+        'bs',
+        'ca',
+        'cs',
+        'cy',
+        'da',
+        'et',
+        'eu',
+        'fi',
+        'gl',
+        'he',
+        'hi',
+        'is',
+        'ja',
+        'ka',
+        'kk',
+        'kn',
+        'lv',
+        'mn',
+        'mr',
+        'ms',
+        'sk',
+        'sl',
+        'sq',
+        'sr_Cyrl',
+        'sr_Latn',
+        'sr_Latn_ME',
+        'sw',
+        'tg',
+        'th',
+        'tl',
+        'uk',
+        'ur',
+    ];
+}

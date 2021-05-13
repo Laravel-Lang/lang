2f56a3216a44b865439719474c7188b7ae93063c
Andrey Helldar <helldar@ai-rus.com>
2021-05-13 23:41:42 +0300

    Added search for Laravel Fortify and Jetstream keys

---
+++ app/main/Processors/Packages.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace LaravelLang\Lang\Processors;
+
+use Helldar\Support\Facades\Helpers\Arr;
+use LaravelLang\Lang\Facades\Package;
+
+final class Packages extends Processor
+{
+    protected array $packages = [
+        'laravel/fortify'   => 'fortify.json',
+        'laravel/jetstream' => 'jetstream.json',
+    ];
+
+    public function run(): void
+    {
+        foreach ($this->packages as $package => $filename) {
+            $items = $this->files($package);
+
+            $path = $this->getSourcePath($filename);
+
+            $content = $this->map($items);
+
+            $this->sort($content);
+
+            $this->store($path, $content);
+        }
+    }
+
+    protected function map(array $items): array
+    {
+        return Arr::renameKeys($items, static fn ($key, $value) => $value);
+    }
+
+    protected function files(string $package): array
+    {
+        $path = $this->vendorPath($package);
+
+        return Package::some()
+            ->path($path)
+            ->content();
+    }
+
+    protected function vendorPath(string $package): string
+    {
+        return $this->app->path('vendor/' . $package);
+    }
+}

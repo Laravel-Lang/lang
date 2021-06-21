a5c65ba6ba6ef88d2e27ea6338b1a520d433f43e
Kimmo Johansson <kimmo.johansson@lendiron.com>
2021-06-21 11:21:09 +0000

    Add missing keys and improve translations

--- locales/fi/auth.php
+++ locales/fi/auth.php
@@ -13,6 +13,6 @@
 
 return [
     'failed'   => 'Kirjautuminen epäonnistui.',
-    'password' => 'The provided password is incorrect.',
+    'password' => 'Annettu salasana on väärin.',
     'throttle' => 'Liian monta kirjautumisyritystä. Yritä uudelleen :seconds sekunnin kuluttua.',
 ];

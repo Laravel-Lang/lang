4b33b0a0cf43a43bee11f0ff348f08cddebaef62
Olsza <olsza@users.noreply.github.com>
2021-07-03 15:40:27 +0200

    Translated into PL of missing sentences in validation-inline.php

--- locales/pl/validation-inline.php
+++ locales/pl/validation-inline.php
@@ -31,7 +31,7 @@ return [
     ],
     'boolean'              => 'Pole musi mieć wartość logiczną prawda albo fałsz.',
     'confirmed'            => 'Potwierdzenie pola nie zgadza się.',
-    'current_password'     => 'The password is incorrect.',
+    'current_password'     => 'Hasło jest nieprawidłowe.',
     'date'                 => 'Pole nie jest prawidłową datą.',
     'date_equals'          => 'Pole musi być datą równą :date.',
     'date_format'          => 'Pole nie jest w formacie :format.',

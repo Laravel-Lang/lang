a2ca2ff3c01a526d188e1434cb821d8ca03bc013
Sayidazim <sayidazimahmudov@gmail.com>
2021-07-29 14:49:42 +0500

    Uzbek latin & cyrillic translation files corrected

--- locales/uz_Latn/auth.php
+++ locales/uz_Latn/auth.php
@@ -13,6 +13,6 @@
 
 return [
     'failed'   => 'Bunday maʼlumotlarga ega foydalanuvchi mavjud emas.',
-    'password' => 'The provided password is incorrect.',
+    'password' => 'Kiritilgan parol noto‘g‘ri.',
     'throttle' => 'Kirish uchun juda ko‘p urinishlar aniqlandi. Iltimos :seconds soniyadan so‘ng yana urinib ko‘ring.',
 ];

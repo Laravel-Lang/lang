3c5bc4cc9b641154079d67a96d1bced1eee1751a
Luis Antonio Parrado <luisprmat@gmail.com>
2021-08-26 12:41:08 -0500

    [es] Add accepted_if validation rule

--- locales/es/validation.php
+++ locales/es/validation.php
@@ -13,7 +13,7 @@
 
 return [
     'accepted'             => ':attribute debe ser aceptado.',
-    'accepted_if'          => 'The :attribute must be accepted when :other is :value.',
+    'accepted_if'          => ':attribute debe ser aceptado cuando :other sea :value.',
     'active_url'           => ':attribute no es una URL válida.',
     'after'                => ':attribute debe ser una fecha posterior a :date.',
     'after_or_equal'       => ':attribute debe ser una fecha posterior o igual a :date.',

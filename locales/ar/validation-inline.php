1840aa75333c3152aeffb8fe5b03fbe472f51206
MohamedSabil83 <me@mohamedsabil83.com>
2021-09-25 02:09:28 +0300

    [ar] Translate 'prohibits' phrase

--- locales/ar/validation-inline.php
+++ locales/ar/validation-inline.php
@@ -101,7 +101,7 @@ return [
     'prohibited'           => 'هذا الحقل محظور.',
     'prohibited_if'        => 'هذا الحقل محظور إذا كان :other هو :value.',
     'prohibited_unless'    => 'هذا الحقل محظور ما لم يكن :other ضمن :values.',
-    'prohibits'            => 'This field prohibits :other from being present.',
+    'prohibits'            => 'هذا الحقل يحظر تواجد الحقل :other.',
     'regex'                => 'الصيغة غير صحيحة.',
     'relatable'            => 'هذا الحقل قد لا يكون مرتبطا بالمصدر المحدد.',
     'required'             => 'هذا الحقل مطلوب.',

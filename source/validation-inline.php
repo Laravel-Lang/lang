d096d2ddd153aa492c03505ebb129e24d47b2fe9
Andrey Helldar <helldar@ai-rus.com>
2021-09-25 18:13:54 +0300

    Revert `source/validation-inline.php`

--- source/validation-inline.php
+++ source/validation-inline.php
@@ -144,16 +144,5 @@ return [
         ],
     ],
 
-    /*
-    |--------------------------------------------------------------------------
-    | Custom Validation Attributes
-    |--------------------------------------------------------------------------
-    |
-    | The following language lines are used to swap our attribute placeholder
-    | with something more reader friendly such as "E-Mail Address" instead
-    | of "email". This simply helps us make our message more expressive.
-    |
-    */
-
     'attributes' => [],
 ];

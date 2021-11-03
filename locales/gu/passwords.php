2d7a01200a5f5e7b1454f36b0cfab61b169bb87a
GitHub Action <action@github.com>
2021-11-03 17:21:21 +0000

    Updating translations keys

--- locales/gu/passwords.php
+++ locales/gu/passwords.php
@@ -1,20 +1,20 @@
 <?php
 
-return [
-    /*
-    |--------------------------------------------------------------------------
-    | Password Reset Language Lines
-    |--------------------------------------------------------------------------
-    |
-    | The following language lines are the default lines which match reasons
-    | that are given by the password broker for a password update attempt
-    | has failed, such as for an invalid token or invalid new password.
-    |
-    */
+/*
+|--------------------------------------------------------------------------
+| Password Reset Language Lines
+|--------------------------------------------------------------------------
+|
+| The following language lines are the default lines which match reasons
+| that are given by the password broker for a password update attempt
+| has failed, such as for an invalid token or invalid new password.
+|
+*/
 
-    'reset' => 'તમારો પાસવર્ડ ફરીથી બદલવામાં આવ્યો છે.',
-    'sent' => 'તમારા પાસવર્ડને બદલવા માટે અમે લિંક સાથે એક ઈમેઈલ આપણે મોકલ્યો છે.',
+return [
+    'reset'     => 'તમારો પાસવર્ડ ફરીથી બદલવામાં આવ્યો છે.',
+    'sent'      => 'તમારા પાસવર્ડને બદલવા માટે અમે લિંક સાથે એક ઈમેઈલ આપણે મોકલ્યો છે.',
     'throttled' => 'Please wait before retrying.',
-    'token' => 'પાસવર્ડ બદલવા માટેનું આ ચિહ્ન અમાન્ય છે.',
-    'user' => 'આ ઈમેઈલ એડ્રેસ સાથે અમે વપરાશ કર્તાને શોધી શકતા નથી.',
+    'token'     => 'પાસવર્ડ બદલવા માટેનું આ ચિહ્ન અમાન્ય છે.',
+    'user'      => 'આ ઈમેઈલ એડ્રેસ સાથે અમે વપરાશ કર્તાને શોધી શકતા નથી.',
 ];
--- locales/gu/validation-inline.php
+++ locales/gu/validation-inline.php
@@ -1,18 +1,17 @@
 <?php
 
+/*
+|--------------------------------------------------------------------------
+| Validation Language Lines
+|--------------------------------------------------------------------------
+|
+| The following language lines contain the default error messages used by
+| the validator class. Some of these rules have multiple versions such
+| as the size rules. Feel free to tweak each of these messages here.
+|
+*/
+
 return [
-
-    /*
-    |--------------------------------------------------------------------------
-    | Validation Language Lines
-    |--------------------------------------------------------------------------
-    |
-    | The following language lines contain the default error messages used by
-    | the validator class. Some of these rules have multiple versions such
-    | as the size rules. Feel free to tweak each of these messages here.
-    |
-    */
-
     'accepted'             => 'This field must be accepted.',
     'accepted_if'          => 'This field must be accepted when :other is :value.',
     'active_url'           => 'This is not a valid URL.',
@@ -26,10 +25,10 @@ return [
     'before'               => 'This must be a date before :date.',
     'before_or_equal'      => 'This must be a date before or equal to :date.',
     'between'              => [
-        'numeric' => 'This value must be between :min and :max.',
-        'file'    => 'This file must be between :min and :max kilobytes.',
-        'string'  => 'This string must be between :min and :max characters.',
         'array'   => 'This content must have between :min and :max items.',
+        'file'    => 'This file must be between :min and :max kilobytes.',
+        'numeric' => 'This value must be between :min and :max.',
+        'string'  => 'This string must be between :min and :max characters.',
     ],
     'boolean'              => 'This field must be true or false.',
     'confirmed'            => 'The confirmation does not match.',
@@ -48,16 +47,16 @@ return [
     'file'                 => 'The content must be a file.',
     'filled'               => 'This field must have a value.',
     'gt'                   => [
-        'numeric' => 'The value must be greater than :value.',
-        'file'    => 'The file size must be greater than :value kilobytes.',
-        'string'  => 'The string must be greater than :value characters.',
         'array'   => 'The content must have more than :value items.',
+        'file'    => 'The file size must be greater than :value kilobytes.',
+        'numeric' => 'The value must be greater than :value.',
+        'string'  => 'The string must be greater than :value characters.',
     ],
     'gte'                  => [
-        'numeric' => 'The value must be greater than or equal to :value.',
-        'file'    => 'The file size must be greater than or equal to :value kilobytes.',
-        'string'  => 'The string must be greater than or equal to :value characters.',
         'array'   => 'The content must have :value items or more.',
+        'file'    => 'The file size must be greater than or equal to :value kilobytes.',
+        'numeric' => 'The value must be greater than or equal to :value.',
+        'string'  => 'The string must be greater than or equal to :value characters.',
     ],
     'image'                => 'This must be an image.',
     'in'                   => 'The selected value is invalid.',
@@ -68,30 +67,30 @@ return [
     'ipv6'                 => 'This must be a valid IPv6 address.',
     'json'                 => 'This must be a valid JSON string.',
     'lt'                   => [
-        'numeric' => 'The value must be less than :value.',
-        'file'    => 'The file size must be less than :value kilobytes.',
-        'string'  => 'The string must be less than :value characters.',
         'array'   => 'The content must have less than :value items.',
+        'file'    => 'The file size must be less than :value kilobytes.',
+        'numeric' => 'The value must be less than :value.',
+        'string'  => 'The string must be less than :value characters.',
     ],
     'lte'                  => [
-        'numeric' => 'The value must be less than or equal to :value.',
-        'file'    => 'The file size must be less than or equal to :value kilobytes.',
-        'string'  => 'The string must be less than or equal to :value characters.',
         'array'   => 'The content must not have more than :value items.',
+        'file'    => 'The file size must be less than or equal to :value kilobytes.',
+        'numeric' => 'The value must be less than or equal to :value.',
+        'string'  => 'The string must be less than or equal to :value characters.',
     ],
     'max'                  => [
-        'numeric' => 'The value may not be greater than :max.',
-        'file'    => 'The file size may not be greater than :max kilobytes.',
-        'string'  => 'The string may not be greater than :max characters.',
         'array'   => 'The content may not have more than :max items.',
+        'file'    => 'The file size may not be greater than :max kilobytes.',
+        'numeric' => 'The value may not be greater than :max.',
+        'string'  => 'The string may not be greater than :max characters.',
     ],
     'mimes'                => 'This must be a file of type: :values.',
     'mimetypes'            => 'This must be a file of type: :values.',
     'min'                  => [
-        'numeric' => 'The value must be at least :min.',
-        'file'    => 'The file size must be at least :min kilobytes.',
-        'string'  => 'The string must be at least :min characters.',
         'array'   => 'The value must have at least :min items.',
+        'file'    => 'The file size must be at least :min kilobytes.',
+        'numeric' => 'The value must be at least :min.',
+        'string'  => 'The string must be at least :min characters.',
     ],
     'multiple_of'          => 'The value must be a multiple of :value',
     'not_in'               => 'The selected value is invalid.',
@@ -99,6 +98,10 @@ return [
     'numeric'              => 'This must be a number.',
     'password'             => 'The password is incorrect.',
     'present'              => 'This field must be present.',
+    'prohibited'           => 'This field is prohibited.',
+    'prohibited_if'        => 'This field is prohibited when :other is :value.',
+    'prohibited_unless'    => 'This field is prohibited unless :other is in :values.',
+    'prohibits'            => 'This field prohibits :other from being present.',
     'regex'                => 'This format is invalid.',
     'relatable'            => 'This field may not be associated with this resource.',
     'required'             => 'This field is required.',
@@ -108,16 +111,12 @@ return [
     'required_with_all'    => 'This field is required when :values are present.',
     'required_without'     => 'This field is required when :values is not present.',
     'required_without_all' => 'This field is required when none of :values are present.',
-    'prohibited'           => 'This field is prohibited.',
-    'prohibited_if'        => 'This field is prohibited when :other is :value.',
-    'prohibited_unless'    => 'This field is prohibited unless :other is in :values.',
-    'prohibits'            => 'This field prohibits :other from being present.',
     'same'                 => 'The value of this field must match the one from :other.',
     'size'                 => [
-        'numeric' => 'The value must be :size.',
-        'file'    => 'The file size must be :size kilobytes.',
-        'string'  => 'The string must be :size characters.',
         'array'   => 'The content must contain :size items.',
+        'file'    => 'The file size must be :size kilobytes.',
+        'numeric' => 'The value must be :size.',
+        'string'  => 'The string must be :size characters.',
     ],
     'starts_with'          => 'This must start with one of the following: :values.',
     'string'               => 'This must be a string.',
@@ -126,4 +125,9 @@ return [
     'uploaded'             => 'This failed to upload.',
     'url'                  => 'This must be a valid URL.',
     'uuid'                 => 'This must be a valid UUID.',
+    'custom'               => [
+        'attribute-name' => [
+            'rule-name' => 'custom-message',
+        ],
+    ],
 ];

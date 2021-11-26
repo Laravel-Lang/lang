eb53ef113d917bc0b1b08730d23a2466af7fb02c
Maximilián Csank <info@limix.eu>
2021-11-26 15:16:11 +0100

    Added translation for incorrect password

--- locales/sk/auth.php
+++ locales/sk/auth.php
@@ -13,6 +13,6 @@
 
 return [
     'failed'   => 'Prihlasovacie údaje nie sú správne.',
-    'password' => 'The provided password is incorrect.',
+    'password' => 'Zadané heslo je nesprávne.',
     'throttle' => 'Prekročený limit pokusov. Skúste znovu o :seconds sekúnd.',
 ];

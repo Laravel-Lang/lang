662c701b7fa55ca1243f7680af940b7697657f86
JappaWakka <jjlspeelman@outlook.com>
2022-03-25 18:23:02 +0100

    Tweaked one line in passwords.php

--- locales/nl/passwords.php
+++ locales/nl/passwords.php
@@ -14,7 +14,7 @@
 return [
     'reset'     => 'Het wachtwoord van uw account is gewijzigd.',
     'sent'      => 'We hebben een e-mail verstuurd met instructies om een nieuw wachtwoord in te stellen.',
-    'throttled' => 'Gelieve even te wachten voor u het opnieuw probeert.',
+    'throttled' => 'Gelieve even te wachten en het dan opnieuw te proberen.',
     'token'     => 'Dit wachtwoordhersteltoken is niet geldig.',
     'user'      => 'Geen gebruiker bekend met het e-mailadres.',
 ];

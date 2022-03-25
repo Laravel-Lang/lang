ac0c70c8c8ed7930cc927e3f64fa47132a8f5589
JappaWakka <jjlspeelman@outlook.com>
2022-03-25 18:17:21 +0100

    Revert "Tweaked Dutch localization"
    This reverts commit bfc5f54e1fee36c5939b58cfa8eeed2229dfc5da.

--- locales/nl/auth.php
+++ locales/nl/auth.php
@@ -14,5 +14,5 @@
 return [
     'failed'   => 'Deze combinatie van e-mailadres en wachtwoord is niet geldig.',
     'password' => 'Het opgegeven wachtwoord is onjuist.',
-    'throttle' => 'Te veel mislukte aanmeldpogingen. Probeer het over nog eens over :seconds seconden.',
+    'throttle' => 'Te veel mislukte loginpogingen. Probeer het over :seconds seconden nogmaals.',
 ];

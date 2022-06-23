c81c1a1cb705a62d1a770d6a1ae949758cf034aa
hs-qwerty <56368718+hs-qwerty@users.noreply.github.com>
2022-06-23 21:32:05 +0300

    az.json update

--- locales/az/auth.php
+++ locales/az/auth.php
@@ -13,6 +13,6 @@
 
 return [
     'failed'   => 'İstifadəçi adı və ya şifrə səhvdir',
-    'password' => 'The provided password is incorrect.',
+    'password' => 'Təqdim olunan parol səhvdir.',
     'throttle' => ':seconds saniyə ərzində yenidən cəhd edin',
 ];

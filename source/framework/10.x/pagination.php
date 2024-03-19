17f17106f14af3692e3059ae2fa94174f953074e
Andrey Helldar <helldar@dragon-code.pro>
2024-03-19 12:27:38 +0300

    [source]: Remove support for Laravel Framework 10, Lumen Framework, Breeze 1, Cashier Stripe 13, 14, Jetstream 2, 3, 4

--- source/framework/10.x/pagination.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-return [
-    'next'     => 'Next &raquo;',
-    'previous' => '&laquo; Previous',
-];
--- src/Plugin.php
+++ src/Plugin.php
@@ -14,29 +14,19 @@ class Plugin extends Provider
 
     protected array $plugins = [
         Plugins\Breeze\Master::class,
-        Plugins\Breeze\V1::class,
         Plugins\Breeze\V2::class,
 
         Plugins\Cashier\Stripe\Master::class,
-        Plugins\Cashier\Stripe\V13::class,
-        Plugins\Cashier\Stripe\V14::class,
         Plugins\Cashier\Stripe\V15::class,
 
         Plugins\Fortify\Master::class,
         Plugins\Fortify\V1::class,
 
         Plugins\Jetstream\Master::class,
-        Plugins\Jetstream\V2::class,
-        Plugins\Jetstream\V3::class,
-        Plugins\Jetstream\V4::class,
         Plugins\Jetstream\V5::class,
 
         Plugins\Laravel\Master::class,
-        Plugins\Laravel\V10::class,
         Plugins\Laravel\V11::class,
-
-        Plugins\Lumen\Master::class,
-        Plugins\Lumen\V10::class,
 
         Plugins\Nova\LogViewer\Main::class,
         Plugins\Nova\V4::class,

f9ca3461deaeeca986153dc0f563b433cb717fa5
andrey-helldar <10347617+andrey-helldar@users.noreply.github.com>
2025-09-18 14:51:07 +0000

    🗝️ Updated Laravel Nova 4.x keys

--- source/nova/4.x/validation.php
+++ source/nova/4.x/validation.php
@@ -1,7 +1,5 @@
 <?php
 
-declare(strict_types=1);
-
 return [
 
     /*
@@ -15,7 +13,7 @@ return [
     |
     */
 
-    'attached'  => 'This :attribute is already attached.',
+    'attached' => 'This :attribute is already attached.',
     'relatable' => 'This :attribute may not be associated with this resource.',
 
 ];

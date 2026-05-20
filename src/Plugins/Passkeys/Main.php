1ab8ae7734b79f33816b440279a109ed0dca478b
Luis Parrado <luisprmat@gmail.com>
2026-05-19 20:47:31 -0500

    [source]: Add laravel/passkeys translations

---
+++ src/Plugins/Passkeys/Main.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace LaravelLang\Lang\Plugins\Passkeys;
+
+use LaravelLang\Publisher\Plugins\Plugin;
+
+class Main extends Plugin
+{
+    protected ?string $vendor = 'laravel/passkeys';
+
+    protected string $version = 'dev-main';
+
+    public function files(): array
+    {
+        return [
+            'passkeys/main/passkeys.json'    => '{locale}.json',
+            'passkeys/extended.json'         => '{locale}.json',
+        ];
+    }
+}

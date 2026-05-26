1cefb80c4bc5e049fd70a29e9328d633b955a29a
Luis Parrado <luisprmat@gmail.com>
2026-05-26 17:30:29 -0500

    Update src/Plugins/Passkeys/Main.php
    Co-authored-by: Andrey Helldar <helldar@dragon-code.pro>
    Signed-off-by: Luis Parrado <luisprmat@gmail.com>

--- src/Plugins/Passkeys/Main.php
+++ src/Plugins/Passkeys/Main.php
@@ -10,7 +10,7 @@ class Main extends Plugin
 {
     protected ?string $vendor = 'laravel/passkeys';
 
-    protected string $version = 'dev-main || ^0.2';
+    protected string $version = 'dev-main || 0.*';
 
     public function files(): array
     {
